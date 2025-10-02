70f93a5a60ce9316815ddac2f86ec3baece23f00
tracer-ysyx <tracer@ysyx.org>
2025-10-03 02:26:16 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  02:26:16 up  4:16,  1 user,  load average: 0.43, 0.74, 0.73

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -31,7 +31,7 @@ bool expr_eval_test_unsigned(char *path) {
   int errcnt = 0;
   bool success = true;
 
-  while (getline(&ln, NULL, stdin) > 0) {
+  while (getline(&ln, NULL, stdin) >= 0) {
     cnt ++;
     fprintf(stderr, "\rTesting case #%6d: \n", cnt);
     word_t expected;
