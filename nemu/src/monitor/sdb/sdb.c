7b73919c96ed3513af5a503e0e52614850e924d9
tracer-ysyx <tracer@ysyx.org>
2025-10-02 17:00:02 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  17:00:02 up 16:26,  1 user,  load average: 0.67, 0.47, 0.50

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -73,7 +73,6 @@ static int cmd_info(char *args) {
       TODO();
       return 0;
     default: 
-      // pass 
       printf("Invalid argument `%c`, type `help info` for more info\n", arg[0]);
       return 1;
   }
