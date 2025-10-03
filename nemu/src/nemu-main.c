75d522736dde61e3dd49126b8842d2b344713762
Yijiang Kong <KongYijiang@stu.xjtu.edu.cn>
2025-10-04 01:27:09 +0800

    Impl expr eval (non-negative) and tested fine

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -13,12 +13,67 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
+#include "monitor/sdb/sdb.h"
+#include <assert.h>
 #include <common.h>
+#include <readline/readline.h>
+#include <stdio.h>
+#include <sys/types.h>
 
 void init_monitor(int, char *[]);
 void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
+
+bool expr_eval_test_unsigned(char *path) {
+  FILE* fp = fopen(path, "r");
+  char* ln = NULL;
+  int cnt = 0; 
+  int errcnt = 0;
+  bool success = true;
+  size_t malloc_sz;
+  ssize_t read_strlen;
+  
+  while ((read_strlen = getline(&ln, &malloc_sz, fp)) > 0) {
+    // printf("Malloc %lu\n", malloc_sz);
+    // printf("%s\n", ln);
+    // printf("%lu\n", strlen(ln));
+    ln[read_strlen-1] = 0;
+    cnt ++;
+    fprintf(stderr, "\rTesting case #%6d: ", cnt);
+    word_t expected;
+    int dig_len;
+    int read_num = sscanf(ln, "%u%n", &expected, &dig_len);
+    // printf("%u %d %d\n", expected, dig_len, read_num);
+    assert(read_num == 1);
+    char* exprstr = ln + dig_len;
+
+    bool succ;
+    word_t ret = expr(exprstr, &succ);
+
+    if (!succ) {
+      fprintf(stderr, "\n[RE:%6d] Expr parse error\n", cnt);
+      fprintf(stderr, "Test Case:\n\"%s\"\n", exprstr);
+      success = false;
+      errcnt ++;
+      return false;
+    } else if (expected != ret) {
+      fprintf(stderr, "\n[WA:%6d] Expr parse error\n", cnt);
+      fprintf(stderr, "Test Case:\n\"%s\"\n", exprstr);
+      fprintf(stderr, "Expected: %u, Read %u\n", expected, ret);
+      success = false;
+      errcnt ++;
+      return false;
+    } else {
+      fprintf(stderr, "[AC:%6d] Pass", cnt);
+    }
+    free(ln);
+    ln = NULL;
+  }
+  fclose(fp);
+  fprintf(stderr, "\n=== Total %d Error %d ===\n", cnt, errcnt);
+  return success;
+}
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
@@ -27,7 +82,8 @@ int main(int argc, char *argv[]) {
 #else
   init_monitor(argc, argv);
 #endif
-
+   
+  return !expr_eval_test_unsigned("tools/gen-expr/input_nemu.txt");
   /* Start engine. */
   engine_start();
 
