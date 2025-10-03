ad0a2e361ca93715487544c80df4fd8c8b80ab1b
tracer-ysyx <tracer@ysyx.org>
2025-10-03 23:42:53 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  23:42:53 up  1:38,  1 user,  load average: 0.92, 0.80, 0.67

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -81,7 +81,7 @@ int main(int argc, char *argv[]) {
   init_monitor(argc, argv);
 #endif
    
-  return expr_eval_test_unsigned("tools/gen-expr/input.txt");
+  return expr_eval_test_unsigned("tools/gen-expr/input_nemu.txt");
   /* Start engine. */
   engine_start();
 
