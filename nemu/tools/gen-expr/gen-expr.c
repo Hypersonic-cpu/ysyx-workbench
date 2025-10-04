6ff9654b7f384c27d1edf43c986d655af4da9498
tracer-ysyx <tracer@ysyx.org>
2025-10-04 16:12:48 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  16:12:48 up 13:50,  1 user,  load average: 0.47, 0.35, 0.39

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -39,13 +39,15 @@ static const char* const ops[] = {
 static void gen_rand_expr(int lim) {
   int wnum = 0;
   if (lim < 10) { 
-    wnum = sprintf(buf+buf_ptr, "%1dU", rand()%10); 
+    wnum = sprintf(buf+buf_ptr, "%s%1dU", 
+                   rand() % 2 ? " -" : " ", rand()%10); 
     buf_ptr += wnum;
     return; 
   }
   switch (rand() % 5) {
     case 0: 
-      wnum = sprintf(buf+buf_ptr, "%u", rand() % 10000); 
+      wnum = sprintf(buf+buf_ptr, "%s%uU", 
+                     rand()%2 ? " -" : " ", rand() % 10000); 
       buf_ptr += wnum;
       break;
     case 1: case 2: 
