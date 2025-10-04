94ddcea3d6c793c885da3ce75343ac7c9761ca76
tracer-ysyx <tracer@ysyx.org>
2025-10-05 00:40:29 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  00:40:29 up 22:17,  1 user,  load average: 0.60, 0.69, 0.47

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -120,8 +120,8 @@ static bool make_token(char *e) {
 
         // printf( ANSI_FG_BLUE "match rules[%d] = \"%s\" at position %d with len %d: %.*s\n" ANSI_NONE,
         //     i, rules[i].regex, position, substr_len, substr_len, substr_start);
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-            i, rules[i].regex, position, substr_len, substr_len, substr_start);
+        // Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+            // i, rules[i].regex, position, substr_len, substr_len, substr_start);
 
         // printf("position %d += %d\n", position, substr_len);
         position += substr_len;
