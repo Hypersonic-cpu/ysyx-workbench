a3e0854d45d1abd766986d6dce2d12ec06b7e222
tracer-ysyx <tracer@ysyx.org>
2025-10-04 16:44:41 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  16:44:41 up 14:21,  1 user,  load average: 0.64, 0.82, 0.88

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -336,8 +336,8 @@ static word_t eval(int l, int r, bool* valid) {
       break;
   }
 
-  // printf("> Join L(%d,%d) %u R(%d,%d) %u Res %u\n", 
-  //        l, pivot_pos-1, lret, pivot_pos+1, r, rret, res);
+  printf("> Join L(%d,%d) %u R(%d,%d) %u Res %u\n", 
+         l, pivot_pos-1, lret, pivot_pos+1, r, rret, res);
   return res;
 }
 
