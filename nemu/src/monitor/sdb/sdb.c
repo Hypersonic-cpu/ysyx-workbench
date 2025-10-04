9d946c875d31c158fca85a6909571d173e9af34c
tracer-ysyx <tracer@ysyx.org>
2025-10-05 01:02:35 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  01:02:35 up 22:39,  1 user,  load average: 0.85, 0.55, 0.49

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -132,12 +132,20 @@ static int cmd_p(char *args) {
 }
 
 static int cmd_w(char *args) {
+  if (args == NULL) {
+    printf("Invalid arguments, type `help w` for more info\n");
+    return 1;
+  }
   int id = watchpoint_set(args);
   printf("Watchpoint %d set: %s\n", id, args);
   return 0;
 }
 
 static int cmd_d(char *args) {
+  if (args == NULL) {
+    printf("Invalid arguments, type `help d` for more info\n");
+    return 1;
+  }
   int id = atoi(args);
   bool success = watchpoint_del(id);
   printf("Watchpoint %d removal %s\n", 
