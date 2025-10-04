60fabf527518b1a4040c9c73b2683eddc84e650b
tracer-ysyx <tracer@ysyx.org>
2025-10-04 16:09:36 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  16:09:36 up 13:46,  1 user,  load average: 0.40, 0.25, 0.38

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -264,7 +264,7 @@ static int choose_pivot(int l, int r, bool* valid) {
 
 // NOTE: Initial *valid should be true.
 static word_t eval(int l, int r, bool* valid) {
-  printf("! Eval (%d, %d) V%d\n", l, r, *valid);
+  // printf("! Eval (%d, %d) V%d\n", l, r, *valid);
   if (!(*valid)) { return 0; }
   if (l > r) {
     *valid = false;
@@ -279,7 +279,7 @@ static word_t eval(int l, int r, bool* valid) {
     return atoi(tokens[l].str);
   } 
   bool bra_ket = check_braket(l, r, valid);
-  printf("Braket (%d, %d) V%d Ret%d\n", l, r, *valid, bra_ket);
+  // printf("Braket (%d, %d) V%d Ret%d\n", l, r, *valid, bra_ket);
   if (!*valid) { return 0; }
   if (bra_ket) { return eval(l+1, r-1, valid); }
   
@@ -336,8 +336,8 @@ static word_t eval(int l, int r, bool* valid) {
       break;
   }
 
-  printf("> Join L(%d,%d) %u R(%d,%d) %u Res %u\n", 
-         l, pivot_pos-1, lret, pivot_pos+1, r, rret, res);
+  // printf("> Join L(%d,%d) %u R(%d,%d) %u Res %u\n", 
+  //        l, pivot_pos-1, lret, pivot_pos+1, r, rret, res);
   return res;
 }
 
