4e74516fdc777cdbd647a2461330e6d3ad4efba9
tracer-ysyx <tracer@ysyx.org>
2025-10-05 16:15:09 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  16:15:09 up  2:22,  1 user,  load average: 0.45, 0.60, 0.50

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -141,7 +141,7 @@ static int cmd_w(char *args) {
   if (success) {
     printf("Watchpoint %d set: %s\n", id, args);
   } else {
-    printf("Watchpoint is not set for eval failed: %s\n", args);
+    printf("Watchpoint is not set for invalid expr: %s\n", args);
   }
   return 0;
 }
