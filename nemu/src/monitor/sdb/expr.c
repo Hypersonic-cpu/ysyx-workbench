5e120257464414ba298df77845da96ffe2fe491e
tracer-ysyx <tracer@ysyx.org>
2025-10-05 16:25:11 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  16:25:11 up  2:32,  1 user,  load average: 0.69, 0.58, 0.52

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -153,10 +153,11 @@ static bool make_token(char *e) {
                      position, e, position, "");
               return false;
             }
+            assert(substr_len > 0);
             tokens[nr_token].type = rules[i].token_type;
             // Skip '\$' character.
             strncpy(tokens[nr_token].str, substr_start+1, substr_len-1);
-            tokens[nr_token].str[substr_len] = '\0';
+            tokens[nr_token].str[substr_len-1] = '\0';
             nr_token++;
             break;
           default:
