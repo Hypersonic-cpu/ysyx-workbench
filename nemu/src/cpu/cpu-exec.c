e784dc7ec1eb53c38a6d61462baa0f07cd0e7bee
tracer-ysyx <tracer@ysyx.org>
2025-10-05 15:26:10 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  15:26:10 up  1:33,  1 user,  load average: 0.53, 0.41, 0.39

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -41,7 +41,8 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
   if (trig_wp()) {
-    nemu_state.state = NEMU_STOP;
+    if (nemu_state.state == NEMU_RUNNING)
+      { nemu_state.state = NEMU_STOP; }
   }
 }
 
