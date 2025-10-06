5cd2e2e821fbd20f80fd00d5147337f1242a4267
Yijiang Kong <KongYijiang@stu.xjtu.edu.cn>
2025-10-06 16:59:21 +0800

    Connect to NVBoard

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -5,36 +5,46 @@
 #include <memory>
 #include <verilated.h>
 #include <verilated_fst_c.h>
+#include <nvboard.h>
 
 #include "Vexample.h"
+
+static TOP_NAME dut;
+void nvboard_bind_all_pins(TOP_NAME* top);
 
 int 
 main(int argc, char* argv[]) {
   printf("[ CXX Wrapper for NPC ]\n");
   
-  const std::unique_ptr<VerilatedContext> contextp { new VerilatedContext };
-  Verilated::traceEverOn(true);
-  Verilated::mkdir("logs");
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
 
-  VerilatedFstC* tfp = new VerilatedFstC;
+  // const std::unique_ptr<VerilatedContext> contextp { new VerilatedContext };
+  // Verilated::traceEverOn(true);
+  // Verilated::mkdir("logs");
+  //
+  // VerilatedFstC* tfp = new VerilatedFstC;
 
-  const std::unique_ptr<Vexample> top { new Vexample{ contextp.get() } };
-  top->trace(tfp, 99);
-  tfp->open("logs/example.fst");
+  // const std::unique_ptr<Vexample> top { new Vexample{ contextp.get() } };
+  // top->trace(tfp, 99);
+  // tfp->open("logs/example.fst");
 
-  while (contextp->time() < 100U && !contextp->gotFinish()) {
-    contextp->timeInc(1);
-    int a = rand() & 1;
-    int b = rand() & 1;
-    top->a = a;
-    top->b = b;
-    top->eval();
-    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-    tfp->dump(contextp->time());
+  // while (contextp->time() < 100U && !contextp->gotFinish()) {
+  while (true) {
+    nvboard_update();
+    dut.eval();
+    // contextp->timeInc(1);
+    // int a = rand() & 1;
+    // int b = rand() & 1;
+    // top->a = a;
+    // top->b = b;
+    // top->eval();
+    // printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+    // tfp->dump(contextp->time());
   }
 
-  top->final();
-  tfp->close();
-  delete tfp;
+  // top->final();
+  // tfp->close();
+  // delete tfp;
   return 0;
 }
