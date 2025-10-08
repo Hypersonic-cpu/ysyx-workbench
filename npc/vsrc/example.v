2c53b3d8df5ccdd253a8651bf96d25d5e3d57e82
Yijiang Kong <KongYijiang@stu.xjtu.edu.cn>
2025-10-08 15:46:19 +0800

    Connect flowing lights to nvboard

--- npc/vsrc/example.v
+++ npc/vsrc/example.v
@@ -1,13 +1,13 @@
 module example(
   input reset,
   input clk,
-  output reg [15:0] leds
+  output reg [15:0] led
 );
   reg [31:0] count;
   always @(posedge clk) begin
-    if (reset) begin leds <= 1; count <= 0; end
+    if (reset) begin led <= 1; count <= 0; end
     else begin
-      if (count == 0) leds <= {leds[14:0], leds[15]};
+      if (count == 0) led <= {led[14:0], led[15]};
       count <= (count >= 5000000 ? 32'b0 : count + 1);
     end
   end
