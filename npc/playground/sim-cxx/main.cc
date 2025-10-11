41cffbccfe6f268a51202558d1584ac91e195d98
Yijiang Kong <KongYijiang@stu.xjtu.edu.cn>
2025-10-12 03:32:15 +0800

    Integrate Verilator(tested) and NVBoard(untested)

---
+++ npc/playground/sim-cxx/main.cc
@@ -0,0 +1,76 @@
+#include <ctime>
+#include <memory>
+#include <cstdlib>
+
+#include <numeric>
+#include <verilated.h>
+#include <verilated_fst_c.h>
+
+#include "VGCD.h"
+// #include "verilatedos.h"
+
+// vluint64_t glb_time { 0 };
+// double sc_time_stamp() { return glb_time; }
+//
+
+inline void 
+single_cycle(
+    const std::unique_ptr<TOP_NAME>& top, 
+    const std::unique_ptr<VerilatedContext>& context) {
+
+  context->timeInc(1);
+  top->clock = 1;
+  top->eval();
+  top->clock = 0;
+  top->eval();
+}
+
+int 
+main() {
+  const std::unique_ptr<VerilatedContext> contextp { new VerilatedContext };
+
+  Verilated::traceEverOn(true);
+  // VerilatedVcdC* tfp = new VerilatedVcdC;
+  VerilatedFstC* tfp = new VerilatedFstC;
+
+  const std::unique_ptr<TOP_NAME> top{new TOP_NAME{contextp.get(), "TOP"}};
+  // Trace 99 levels of hierarchy (or see below)
+  top->trace(tfp, 99);
+                        // tfp->dumpvars(1, "t"); // trace 1 level under "t"
+  tfp->open("/mnt/hgfs/Arch-PA/ysyx-workbench/npc/build-sim/playground/logs/log.fst");
+  
+  int passed = 0;
+  int failed = 0;
+
+  constexpr vluint64_t TimeMax = 100U;
+  srand(time(0));
+  while (contextp->time() < TimeMax && !contextp->gotFinish()) {
+    int a = rand() % 15 + 1;
+    int b = rand() % 15 + 1;
+    top->io_value1 = a;
+    top->io_value2 = b;
+    top->io_loadingValues = 1;
+    single_cycle(top, contextp);
+    single_cycle(top, contextp);
+    top->io_loadingValues = 0;
+    while (!top->io_outputValid && contextp->time() < TimeMax) {
+      printf("%lu: a = %d, b = %d, GCD = %d\n", contextp->time(), a, b, top->io_outputGCD);
+      single_cycle(top, contextp);
+      tfp->dump(contextp->time());
+    }
+    if (contextp->time() < TimeMax) {
+      printf("a = %d, b = %d, GCD = %d\n", a, b, top->io_outputGCD);
+      if (top->io_outputGCD == std::gcd(a, b)) { passed ++; }
+      else { failed ++; }
+    } else {
+      printf("a = %d, b = %d, Timeout", a, b);
+      failed ++;
+    }
+    tfp->dump(contextp->time());
+    if (passed + failed) { break; }
+  }
+
+  top->final();
+  tfp->close();
+  return 0;
+}
