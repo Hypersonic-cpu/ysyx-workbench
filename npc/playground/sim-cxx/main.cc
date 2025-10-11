e1198db1d97c2035bba552248efb258b656ca7b5
tracer-ysyx <tracer@ysyx.org>
2025-10-12 02:35:46 +0800

    > generate verilog ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  02:35:46 up  6:49,  1 user,  load average: 0.43, 0.41, 0.53

---
+++ npc/playground/sim-cxx/main.cc
@@ -0,0 +1,55 @@
+#include <memory>
+#include <cstdlib>
+
+#include <verilated.h>
+#include <verilated_fst_c.h>
+
+#include "VGCD.h"
+#include "verilatedos.h"
+
+// vluint64_t glb_time { 0 };
+// double sc_time_stamp() { return glb_time; }
+
+int 
+main() {
+  const std::unique_ptr<VerilatedContext> contextp { new VerilatedContext };
+
+  Verilated::traceEverOn(true);
+  // VerilatedVcdC* tfp = new VerilatedVcdC;
+  VerilatedFstC* tfp = new VerilatedFstC;
+
+  const std::unique_ptr<TOP_NAME> top{new TOP_NAME{contextp.get(), "TOP"}};
+  // Trace 99 levels of hierarchy (or see below)
+  top->trace(tfp, 99);
+                        // tfp->dumpvars(1, "t"); // trace 1 level under "t"
+  tfp->open("logs/log.fst");
+  
+  int passed = 0;
+  int failed = 0;
+
+  constexpr vluint64_t TimeMax = 100U;
+  while (contextp->time() < TimeMax && !contextp->gotFinish()) {
+    contextp->timeInc(1);
+    int a = rand() % 16;
+    int b = rand() & 16;
+    top->io_value1 = a;
+    top->io_value2 = b;
+    top->io_loadingValues = 1;
+    top->eval();
+    while (!top->io_outputValid && contextp->time() < TimeMax) {
+      top->eval();
+    }
+    if (contextp->time() < TimeMax) {
+      printf("a = %d, b = %d, GCD = %d\n", a, b, top->io_outputGCD);
+      passed++;
+    } else {
+      failed++;
+    }
+    tfp->dump(contextp->time());
+    if (passed + failed) { break; }
+  }
+
+  top->final();
+  tfp->close();
+  return 0;
+}
