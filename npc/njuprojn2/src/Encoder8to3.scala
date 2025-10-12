c010a904a343ee173ee1def60b3dc727136b80e2
tracer-ysyx <tracer@ysyx.org>
2025-10-12 17:07:00 +0800

    > generate verilog ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  17:07:00 up  1:39,  1 user,  load average: 0.77, 0.47, 0.40

--- npc/njuprojn2/src/Encoder8to3.scala
+++ npc/njuprojn2/src/Encoder8to3.scala
@@ -4,5 +4,8 @@ import chisel3._
 import chisel3.util._
 
 class Encoder8to3 extends Module {
-  io = IO(new Bundle{})
+  val io = IO(new Bundle{
+    val in    = Input(Uint(8.W))
+    val out   = Input(Uint(8.W))
+  })
 }
