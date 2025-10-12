3d99cefbaedc5265543b1458a1556a9676e53f6d
tracer-ysyx <tracer@ysyx.org>
2025-10-12 17:21:03 +0800

    > generate verilog ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  17:21:03 up  1:53,  1 user,  load average: 0.80, 0.77, 0.60

--- npc/njuprojn2/src/Encoder8to3.scala
+++ npc/njuprojn2/src/Encoder8to3.scala
@@ -6,7 +6,7 @@ import chisel3.util._
 class Encoder8to3 extends Module {
   val io = IO(new Bundle{
     val in    = Input(UInt(8.W))
-    val out   = Input(UInt(3.W))
+    val out   = Output(UInt(3.W))
   })
 
   val cum = Wire(Vec(8, UInt(3.W)))
@@ -14,6 +14,5 @@ class Encoder8to3 extends Module {
   for (i <- 1 until 8) {
     cum(i) := Mux(io.in(i), i.U, 0.U) | cum(i-1)
   }
-  val out = cum(7)
-  io.out := out
+  io.out := cum(7)
 }
