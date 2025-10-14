0f4528ccc8b6d8b3f7382f1dcffb2aa55b9da51f
tracer-ysyx <tracer@ysyx.org>
2025-10-14 23:17:41 +0800

    > generate verilog ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  23:17:41 up  3:26,  1 user,  load average: 0.23, 0.63, 0.84

--- npc/njuprojn6/sim-nvboard/main.cc
+++ npc/njuprojn6/sim-nvboard/main.cc
@@ -8,13 +8,17 @@ static TOP_NAME dut;
 void nvboard_bind_all_pins(TOP_NAME* top);
 
 static void single_cycle() {
-  dut.clock = 0; dut.eval();
-  dut.clock = 1; dut.eval();
+  dut.eval()
+  // dut.clock = 0; dut.eval();
+  // dut.clock = 1; dut.eval();
 }
 
 static void reset(int n) {
   dut.reset = 1;
-  while (n -- > 0) single_cycle();
+  while (n -- > 0) {
+    dut.clock = 1; dut.eval();
+    dut.clock = 0; dut.eval();
+  }
   dut.reset = 0;
 }
 
