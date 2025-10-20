e8b4d9a2033e10db7c533abe0b00f9a1437ef63e
tracer-ysyx <tracer@ysyx.org>
2025-10-20 22:21:51 +0800

    > generate verilog ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  22:21:51 up  3:43,  1 user,  load average: 0.60, 0.48, 0.69

--- npc/njuprojn7/sim-nvboard/main.cc
+++ npc/njuprojn7/sim-nvboard/main.cc
@@ -3,9 +3,6 @@
 #include <thread>
 #include "VPs2DetectorFpga.h"
 
-#include <verilated.h>
-#include <verilated_fst_c.h>
-
 static TOP_NAME dut;
 
 void nvboard_bind_all_pins(TOP_NAME* top);
@@ -24,22 +21,13 @@ static void reset(int n) {
 // constexpr auto SleepTime = std::chrono::milliseconds(50);
 
 int main() {
-  const std::unique_ptr<VerilatedContext> contextp { new VerilatedContext };
-  Verilated::traceEverOn(true);
-  VerilatedFstC* tfp = new VerilatedFstC;
-  dut.trace(tfp, 99);
-  tfp->open("/mnt/hgfs/Arch-PA/ysyx-workbench/npc/build-sim/njuprojn7/log.fst");
-
   nvboard_bind_all_pins(&dut);
-
   nvboard_init();
 
   reset(10);
 
   while(1) {
     nvboard_update();
-    contextp->timeInc(1);
     single_cycle();
-    tfp->dump(contextp->time());
   }
 }
