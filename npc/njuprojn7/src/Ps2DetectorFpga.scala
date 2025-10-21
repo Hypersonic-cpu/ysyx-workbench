e7f8f6f09ca3b68aa9af3f5586ee4962d8ab2e44
tracer-ysyx <tracer@ysyx.org>
2025-10-21 19:28:49 +0800

    > generate verilog ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  19:28:49 up 29 min,  1 user,  load average: 2.10, 1.46, 1.03

--- npc/njuprojn7/src/Ps2DetectorFpga.scala
+++ npc/njuprojn7/src/Ps2DetectorFpga.scala
@@ -66,7 +66,7 @@ class Ps2DetectorFpga extends Module {
   segDecode(3).io.ena := pressState
   segDecode(2).io.ena := pressState
   
-  // printf(cf"$pressState%d, Hi = ${currOut(1)}%x, Lo = ${currOut(0)}%x\n")
+  printf(cf"$pressState%d, Hi = ${currOut(1)}%x, Lo = ${currOut(0)}%x\n")
   switch (pressState) {
     is (true.B) {
       val released = (currOut(1) === 0xF0.U) //  & (currOut(0) === keycodeState)
