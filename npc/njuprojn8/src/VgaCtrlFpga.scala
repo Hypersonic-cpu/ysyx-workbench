320534b58e42085224277c13144954d328065db9
tracer-ysyx <tracer@ysyx.org>
2025-10-21 15:54:48 +0800

    > generate verilog ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  15:54:48 up  3:12,  1 user,  load average: 0.45, 0.49, 0.32

--- npc/njuprojn8/src/VgaCtrlFpga.scala
+++ npc/njuprojn8/src/VgaCtrlFpga.scala
@@ -6,6 +6,24 @@ import chisel3.experimental._
 
 class VgaCtrlFpga extends Module {
   val io = IO(new Bundle {
+    val syncHor = Output(Bool())
+    val syncVer = Output(Bool())
+    val blankN  = Output(Bool())
+    val vgaRGB  = Output(Vec(3, UInt(8.W)))
   })
 
+  val vgaCtrl = Module(new VgaCtrl())
+  // maybe_unused
+  val xPos = vgaCtrl.io.posHor
+  val yPos = vgaCtrl.io.posVer
+  // TODO: should be F(xpos, ypos)
+  vgaCtrl.io.rawData := 0xff0000.U
+
+  io.syncHor := vgaCtrl.io.syncHor
+  io.syncVer := vgaCtrl.io.syncVer
+  io.blankN  := vgaCtrl.io.oValid
+
+  for (i <- 0 until 3) {
+    io.vgaRGB(i) := vgaCtrl.io.oRGB(i)
+  }
 }
