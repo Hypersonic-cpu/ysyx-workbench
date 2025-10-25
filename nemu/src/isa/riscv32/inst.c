0004faad008438a683a46f6c1f4e64bc302a4f02
tracer-ysyx <tracer@ysyx.org>
2025-10-26 02:35:44 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  02:35:44 up 14:32,  1 user,  load average: 1.46, 1.02, 0.85

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -187,13 +187,28 @@ static int decode_exec(Decode *s) {
   //             ((uint64_t)src1 * (uint64_t)src2) >> 32
   //         ));
   INSTPAT("0000001 ????? ????? 100 ????? 01100 11", 
-          div    , R, R(rd) = (word_t) ((sword_t)src1 / (sword_t)src2));
+          div    , R, 
+          if ((sword_t)src2 == 0) { 
+              R(rd) = -1;
+          } else if ((sword_t)src2 == -1 && (sword_t)src1 == INT32_MIN) {
+              R(rd) = (word_t) INT32_MIN;
+          } else {
+              R(rd) = (word_t) ((sword_t)src1 / (sword_t)src2);
+          });
   INSTPAT("0000001 ????? ????? 101 ????? 01100 11", 
-          divu   , R, R(rd) = src1 / src2);
+          divu   , R, R(rd) = (src2==0) ? (word_t)(-1) : src1/src2);
   INSTPAT("0000001 ????? ????? 110 ????? 01100 11", 
-          rem    , R, R(rd) = (word_t) ((sword_t)src1 % (sword_t)src2));
+          rem    , R, 
+          if ((sword_t) src2 == 0) {
+              R(rd) = src1;
+          } else if ((sword_t)src2 == -1 && (sword_t)src1 == INT32_MIN) {
+              R(rd) = 0;
+          } else {
+              R(rd) = (word_t) ((sword_t)src1 % (sword_t)src2); 
+          });
   INSTPAT("0000001 ????? ????? 111 ????? 01100 11", 
-          remu   , R, R(rd) = src1 % src2);
+          remu   , R, 
+          R(rd) = (src2 == 0U) ? src1 : (src1 % src2));
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", 
           inv    , N, INV(s->pc));
   INSTPAT_END();
