e3f5b5252b64c4a24db7f9affe7012ef73cdc327
tracer-ysyx <tracer@ysyx.org>
2025-10-25 11:21:14 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  11:21:14 up  1:29,  1 user,  load average: 1.09, 0.63, 0.44

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -80,7 +80,7 @@ expr_eval_test_unsigned(const char *const path)
   return success;
 }
 
-static size_t const TEST_NUMS = 1;
+static size_t const TEST_NUMS = 3;
 static const char* const test_files[] = {
   // "tools/gen-expr/input_all_arith_3251_nemu.txt", 
   "tools/gen-expr/input_nemu.txt",
