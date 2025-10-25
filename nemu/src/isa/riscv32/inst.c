99aed256b5ebdc427ad2120a947060c1ab39fa16
tracer-ysyx <tracer@ysyx.org>
2025-10-26 01:22:14 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  01:22:14 up 13:18,  1 user,  load average: 0.19, 0.28, 0.51

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -170,12 +170,12 @@ static int decode_exec(Decode *s) {
   /** RV32M Extension */
   INSTPAT("0000001 ????? ????? 000 ????? 01100 11", 
           mul    , R, R(rd) = src1 * src2);
-  // INSTPAT("0000001 ????? ????? 001 ????? 01100 11", 
-  //         mulh   , R, 
-  //         R(rd) = (word_t) (
-  //             ((int64_t)((sword_t)src1) * (int64_t)((sword_t)src2))
-  //             >> 32
-  //         ));
+  INSTPAT("0000001 ????? ????? 001 ????? 01100 11", 
+          mulh   , R, 
+          R(rd) = (word_t) (
+              ((int64_t)((sword_t)src1) * (int64_t)((sword_t)src2))
+              >> 32
+          ));
   // INSTPAT("0000001 ????? ????? 010 ????? 01100 11", 
   //         mulhsu , R,
   //         R(rd) = (word_t) (
