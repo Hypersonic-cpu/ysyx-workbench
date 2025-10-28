dde9279b95b9fe352093201aa8afdfa59139918c
tracer-ysyx <tracer@ysyx.org>
2025-10-29 00:02:39 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  00:02:39 up  3:05,  1 user,  load average: 0.39, 0.37, 0.34

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -9,7 +9,8 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  // panic("Not implemented");
+  return NULL;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
