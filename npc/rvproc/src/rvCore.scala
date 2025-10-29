7202307e2a4bd8cd0e5068e4329736a2b28ccd21
tracer-ysyx <tracer@ysyx.org>
2025-10-30 01:25:57 +0800

    > generate verilog ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  01:25:57 up  1:08,  1 user,  load average: 2.33, 1.58, 1.18

--- npc/rvproc/src/rvCore.scala
+++ npc/rvproc/src/rvCore.scala
@@ -259,6 +259,7 @@ class rvCore(romFile: String) extends Module {
   // NOTE: No such inst that stores a calculated result.
   iLsu.io.addr := res
   iLsu.io.data := rs2V
+  iLsu.io.ldEn := false.B // TODO: 
   iLsu.io.wrEn := iDec.io.memWr
   // LSU out
   val loadV = iLsu.io.load
