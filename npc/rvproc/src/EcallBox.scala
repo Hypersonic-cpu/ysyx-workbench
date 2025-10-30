320d93bcc7521e271c44a03674adb99345478c08
tracer-ysyx <tracer@ysyx.org>
2025-10-30 23:13:16 +0800

    > generate verilog ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  23:13:16 up  4:37,  1 user,  load average: 0.46, 0.73, 0.87

--- npc/rvproc/src/EcallBox.scala
+++ npc/rvproc/src/EcallBox.scala
@@ -15,5 +15,5 @@ class EcallBox extends BlackBox with HasBlackBoxResource {
     val a10in = Input(Tp.RegType())
   })
   
-  addResource("/mnt/hgfs/Arch-PA/ysyx-workbench/npc/rvproc/dpic/EcallBox.sv")
+  addResource("/vsrc/EcallBox.sv")
 }
