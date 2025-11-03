0195263ff20e8464de4c4f0f840441e3832ad334
tracer-ysyx <tracer@ysyx.org>
2025-11-03 21:34:29 +0800

    > test RTL ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  21:34:29 up 38 min,  1 user,  load average: 2.18, 1.63, 1.22

--- npc/rvproc/src/rvCore.scala
+++ npc/rvproc/src/rvCore.scala
@@ -263,7 +263,7 @@ class rvCore() extends Module {
   val pc     = RegInit(0.U(ISA.PCBits.W))
   val iReg   = Module(new RegFile())
 
-  printf(cf"[ PC = ${pc}%8x ]\n")
+  printf(cf"[ PC = ${pc}%x ]\n")
   // Func
   val iDec   = Module(new IDU())
   val iExe   = Module(new EXU()) 
