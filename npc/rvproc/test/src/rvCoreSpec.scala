0c8ab95386870bed21a883e7391310a80c49d30f
tracer-ysyx <tracer@ysyx.org>
2025-11-04 00:11:30 +0800

    > test RTL ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  00:11:30 up  3:15,  1 user,  load average: 0.64, 1.00, 0.78

--- npc/rvproc/test/src/rvCoreSpec.scala
+++ npc/rvproc/test/src/rvCoreSpec.scala
@@ -143,42 +143,42 @@ class rvCoreSpec extends AnyFlatSpec with ChiselScalatestTester {
     }
   }
 
-  // "rvCore" should "pass Lui" in {
-  //   PathCfg.doLinkRam("lui.hex")
-  //   test (new rvProc.rvCore())
-  //     .withAnnotations(Seq(
-  //       // WriteVcdAnnotation,
-  //       VerilatorBackendAnnotation,
-  //       VerilatorOpGen.getFlags(),
-  //     )
-  //   ) { dut =>
-  //     dut.io.regPin.poke(1)
-  //     dut.clock.step()
-  //     dut.io.regPrb.expect(0)
-  //
-  //     dut.io.regPin.poke(2)
-  //     dut.clock.step()
-  //     dut.io.regPrb.expect(IntCvt.twosC(4096))
-  //
-  //     dut.io.regPin.poke(3)
-  //     dut.clock.step()
-  //     dut.io.regPrb.expect(0x12345000)
-  //
-  //     dut.io.regPin.poke(4)
-  //     dut.clock.step()
-  //     dut.io.regPrb.expect(-(1 << 31))
-  //
-  //     dut.io.regPin.poke(5)
-  //     dut.clock.step()
-  //     dut.io.regPrb.expect(0x7ffff000)
-  //
-  //     dut.clock.step()
-  //
-  //     dut.io.regPin.poke(15)
-  //     dut.clock.step()
-  //     dut.io.regPrb.expect(0x7AAAA000)
-  //   }
-  // }
+  "rvCore" should "pass Lui" in {
+    PathCfg.doLinkRam("lui.hex")
+    test (new rvProc.rvCore())
+      .withAnnotations(Seq(
+        // WriteVcdAnnotation,
+        VerilatorBackendAnnotation,
+        VerilatorOpGen.getFlags(),
+      )
+    ) { dut =>
+      dut.io.regPin.poke(1)
+      dut.clock.step()
+      dut.io.regPrb.expect(0)
+
+      dut.io.regPin.poke(2)
+      dut.clock.step()
+      dut.io.regPrb.expect(IntCvt.twosC(4096))
+
+      dut.io.regPin.poke(3)
+      dut.clock.step()
+      dut.io.regPrb.expect(0x12345000)
+
+      dut.io.regPin.poke(4)
+      dut.clock.step()
+      dut.io.regPrb.expect(-(1 << 31))
+
+      dut.io.regPin.poke(5)
+      dut.clock.step()
+      dut.io.regPrb.expect(0x7ffff000)
+
+      dut.clock.step()
+
+      dut.io.regPin.poke(15)
+      dut.clock.step()
+      dut.io.regPrb.expect(0x7AAAA000)
+    }
+  }
   //
   // "rvCore" should "exit at Ebreak" in {
   //   PathCfg.doLinkRam("ebreak.hex")
