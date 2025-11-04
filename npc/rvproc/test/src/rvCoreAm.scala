a57feb427a6b646fc8d98351b216058157a572ce
tracer-ysyx <tracer@ysyx.org>
2025-11-04 17:51:43 +0800

    > run with AM ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  17:51:43 up  5:54,  1 user,  load average: 1.89, 1.27, 0.97

--- npc/rvproc/test/src/rvCoreAm.scala
+++ npc/rvproc/test/src/rvCoreAm.scala
@@ -20,7 +20,7 @@ class rvCoreAm extends AnyFlatSpec with ChiselScalatestTester {
       .withAnnotations(Seq(
       WriteVcdAnnotation,
       VerilatorBackendAnnotation,
-      VerilatorOpGen.getFlags(false)
+      VerilatorOpGen.getFlags(true)
     )) { dut =>
       // dut.io.regPin.poke(10)
       dut.clock.setTimeout(7000)
