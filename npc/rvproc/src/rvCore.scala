265cb0eb86f0b0328a6d043eb62eed1aeab83169
tracer-ysyx <tracer@ysyx.org>
2025-11-04 11:36:39 +0800

    > test RTL ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  11:36:39 up 47 min,  1 user,  load average: 0.96, 0.90, 0.74

--- npc/rvproc/src/rvCore.scala
+++ npc/rvproc/src/rvCore.scala
@@ -274,8 +274,8 @@ class LSU extends Module {
   iMem.io.pcin  := io.pcin
   iMem.io.addr  := io.addr
   iMem.io.data  := MuxLookup(lenOp, 0.U) (Seq(
-    MemLenOp.Byte -> (io.data(7, 0) << (io.addr(1,0) << 3.W)),
-    MemLenOp.Half -> (io.data(15,0) << (io.addr(1,1) << 4.W)),
+    MemLenOp.Byte -> (io.data(7, 0) << (io.addr(1,0) << 3.U)),
+    MemLenOp.Half -> (io.data(15,0) << (io.addr(1,1) << 4.U)),
     MemLenOp.Word -> io.data
   ))
   iMem.io.byteMask := MuxLookup(lenOp, 0.U) (
