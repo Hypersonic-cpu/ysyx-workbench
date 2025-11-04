aafaf9a44550b6f1693a839c94bdbea644eecf89
tracer-ysyx <tracer@ysyx.org>
2025-11-04 13:34:17 +0800

    > test RTL ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  13:34:17 up  1:48,  1 user,  load average: 2.38, 1.60, 1.09

--- npc/rvproc/test/src/rvCoreSpec.scala
+++ npc/rvproc/test/src/rvCoreSpec.scala
@@ -286,7 +286,7 @@ class rvCoreSpec extends AnyFlatSpec with ChiselScalatestTester {
     ) { dut =>
       dut.io.regPin.poke(10)
       try {
-        dut.clock.step(60)
+        dut.clock.step(6)
       } catch {
         case e: StopException => {
           println(s"Stop at cycle ${e.cycles}")
