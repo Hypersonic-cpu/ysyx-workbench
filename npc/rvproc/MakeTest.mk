6c9e6e94adb9d27c37a8cbeb65753a29466dc678
tracer-ysyx <tracer@ysyx.org>
2025-11-04 13:20:11 +0800

    > test RTL ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  13:20:11 up  1:34,  1 user,  load average: 1.18, 0.63, 0.52

--- npc/rvproc/MakeTest.mk
+++ npc/rvproc/MakeTest.mk
@@ -1,7 +1,9 @@
 ASMDIR ?= $(PRJ)/prog-gen
 HEXDIR ?= $(PRJ)/prog-rom
 ROMASM  = $(wildcard $(ASMDIR)/*.S)
-ROMHEX  = $(patsubst $(ASMDIR)/%.S, $(HEXDIR)/%.hex, $(ROMASM))
+ROMHV3  = $(wildcard $(ASMDIR)/*.hexv3)
+ROMHEX := $(patsubst $(ASMDIR)/%.S, $(HEXDIR)/%.hex, $(ROMASM))
+ROMHEX += $(patsubst $(ASMDIR)/%.hexv3, $(HEXDIR)/%_v3.hex, $(ROMHV3))
 RV64PF := riscv64-linux-gnu-
 RVCC   := $(RV64PF)gcc
 RVDUMP := $(RV64PF)objdump
@@ -9,6 +11,10 @@ RVDUMP := $(RV64PF)objdump
 .PHONY: genrom
 
 genrom: $(ROMHEX)
+
+$(HEXDIR)/%_v3.hex: $(ASMDIR)/%.hexv3
+	@cat $< | tail -n +2 | sed -E 's/^[0-9a-zA-Z]+: //' | sed -E 's/ /\n/g' > $@
+
 
 $(HEXDIR)/%.hex: $(ASMDIR)/%.asm
 	@echo 'Grep-ing hex into >' $@
@@ -18,4 +24,4 @@ $(ASMDIR)/%.asm: $(ASMDIR)/%.elf
 	$(RVDUMP) -d $^ > $@
 
 $(ASMDIR)/%.elf: $(ASMDIR)/%.S
-	$(RVCC) -nostdlib -march=rv32i -mabi=ilp32 $^ -o $@
+	$(RVCC) -O0 -g -nostdlib -march=rv32i -mabi=ilp32 $^ -o $@
