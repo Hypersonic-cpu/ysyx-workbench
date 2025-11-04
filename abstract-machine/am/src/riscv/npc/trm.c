a3a2fd42c43987794219d477ab66fc3ca7942122
tracer-ysyx <tracer@ysyx.org>
2025-11-04 17:48:36 +0800

    > run with AM ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  17:48:36 up  5:51,  1 user,  load average: 0.92, 0.88, 0.81

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -15,12 +15,7 @@ void putch(char ch) {
 }
 
 void halt(int code) {
-  while (1) {
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-  }
+  asm volatile ("ebreak");
 }
 
 void _trm_init() {
