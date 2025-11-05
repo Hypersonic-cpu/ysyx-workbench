ae735cf5c0727b42625743b9656e1ce712ca51e4
tracer-ysyx <tracer@ysyx.org>
2025-11-06 00:04:50 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  00:04:50 up  1:47,  1 user,  load average: 0.53, 0.84, 0.76

--- nemu/include/isa.h
+++ nemu/include/isa.h
@@ -27,6 +27,8 @@ typedef concat(__GUEST_ISA__, _ISADecodeInfo) ISADecodeInfo;
 // monitor
 extern unsigned char isa_logo[];
 void init_isa();
+// INIT_ELF
+void init_elf(const char* elf_file);
 
 // reg
 extern CPU_state cpu;
--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -13,8 +13,18 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
+#include "common.h"
 #include <isa.h>
 #include <memory/paddr.h>
+
+#include <elf.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/cdefs.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 // this is not consistent with uint8_t
 // but it is ok since we do not access the array directly
@@ -41,3 +51,63 @@ void init_isa() {
   /* Initialize this virtual computer system. */
   restart();
 }
+
+// INIT_ELF
+void init_elf(const char* elf_file) {
+  if (elf_file == NULL) { return; }
+
+  int fd = open(elf_file, O_RDONLY);
+  Assert(fd >= 0, "Can not open '%s'", elf_file);
+
+  struct stat st;
+  int fs_status = fstat(fd, &st);
+  Assert(fs_status == 0, "Cannot get size of elf '%s'", elf_file);
+  Log("The elf is %s, size = %ld", elf_file, st.st_size);
+
+  uint8_t* map = (uint8_t *) mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+  Assert(map != MAP_FAILED, "Elf '%s' mmap failed", elf_file);
+
+  // TODO: 
+  // MUXDEF(CONFIG_ISA64, X, Y);
+
+  // NOTE: Only for RV32 now
+  Elf32_Ehdr* ehdr = (Elf32_Ehdr*) map;
+  Assert(memcmp(ehdr->e_ident, ELFMAG, SELFMAG) == 0,
+         "Elf header mismatch");
+  Assert(ehdr->e_machine == EM_RISCV,
+         "Elf ISA mismatch, not RV32");
+  Assert(ehdr->e_ident[EI_DATA] == ELFDATA2LSB, 
+         "Elf endianess mismatch");
+
+  // Section header
+  Elf32_Shdr* shdr = (Elf32_Shdr*) (map + ehdr->e_shoff);
+  Elf32_Sym* sym_table = NULL;
+  uint8_t* str_table = NULL;
+  unsigned sym_count = 0;
+
+  for (unsigned i = 0; i < ehdr->e_shnum; ++i) {
+    if (shdr[i].sh_type != SHT_SYMTAB) { continue; }
+    sym_table = (Elf32_Sym *) (map + shdr[i].sh_offset);
+    sym_count = shdr[i].sh_size / sizeof(Elf32_Sym);
+    Assert(shdr[i].sh_link < ehdr->e_shnum, 
+           "String table out of bound");
+    str_table = map + shdr[shdr[i].sh_link].sh_offset;
+    break;
+  }
+
+  Assert(sym_table, "Elf symbol table not found");
+  Assert(str_table, "Elf string table not found");
+
+  printf(" === ELF Symbol Table (%u total) === \n", sym_count);
+  for (unsigned i = 0; i < sym_count; ++i) {
+    __attribute_maybe_unused__ int bind = ELF32_ST_BIND(sym_table[i].st_info);
+    __attribute_maybe_unused__ int type = ELF32_ST_TYPE(sym_table[i].st_info);
+
+    printf("[%2u] 0x%8x: %s\n" , i, 
+           (vaddr_t) sym_table[i].st_value, 
+           (const char*) (str_table + sym_table[i].st_name)
+           );
+
+  }
+}
+
