6a5ab073dae6798d41b47d5f833514e1e2608bce
tracer-ysyx <tracer@ysyx.org>
2025-11-06 02:49:47 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  02:49:47 up  4:32,  1 user,  load average: 0.58, 0.45, 0.49

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -69,6 +69,7 @@ static void ftrace(vaddr_t jtar, int rd, vaddr_t snpc) {
     printf("+Jump to addr 0x%8x, symidx %u\n", jtar, idx);
     // Cannot find in symbol table
     if (idx >= symbols.sym_num) { return; }
+    printf("+Jump\n");
     unsigned sp = frames.num++;
     frames.stack[sp].fn = jtar;
     frames.stack[sp].ra = snpc;
