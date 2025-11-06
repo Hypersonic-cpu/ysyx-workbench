1cccb77145d926d5f779e3156dd6c45d59cccd06
tracer-ysyx <tracer@ysyx.org>
2025-11-06 15:28:52 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  15:28:52 up  3:04,  1 user,  load average: 0.99, 0.95, 0.80

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -22,7 +22,7 @@
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   unsigned const regnum = MUXDEF(CONFIG_RVE, 16, 32);
   bool success = true;
-  if (ref_r->pc != pc) {
+  if (ref_r->pc + 4U != pc) {
     fprintf(stderr, ANSI_FG_RED 
             "DiffTest PC mismatch: " 
             "ref " FMT_PADDR " got " FMT_PADDR "\n" ANSI_NONE,
