a2a3cf340b977286ba68a67942338a3b3376d266
tracer-ysyx <tracer@ysyx.org>
2025-11-06 23:17:16 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  23:17:16 up  7:01,  1 user,  load average: 0.80, 0.81, 0.60

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,26 +5,50 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+#define PRINT_BUF_LEN 4096
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  char buffer[PRINT_BUF_LEN] = {0};
+  va_list args;
+  va_start(args, fmt);
+  int ret = vsnprintf(buffer, PRINT_BUF_LEN, fmt, args);
+  va_end(args);
+  putstr(buffer);
+  return ret;
 }
 
+// TODO: Move all main logics into vnsprintf 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  return vsnprintf(out, /* size_t */ -1, fmt, ap);
 }
 
 int sprintf(char *out, const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  int ret = vsprintf(out, fmt, args);
+  va_end(args);
+  return ret;
+}
+
+int snprintf(char *out, size_t n, const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  int ret = vsnprintf(out, n, fmt, args);
+  va_end(args);
+  return ret;
+}
+
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
   char* const orig_out = out;
   /** 0:text, 1:after% */
   unsigned char state = 0;
-  va_list args;
-  va_start(args, fmt);
+  size_t cnt = 0;
   while (*fmt != '\0') {
     if (state == 0) {
       // text before current pos 
       if (*fmt == '%') {
         state = 1;
       } else {
+        if (cnt++ + 1 >= n) { goto vnfinish; }
         *out++ = *fmt;
       }
     } else {
@@ -35,12 +59,13 @@ int sprintf(char *out, const char *fmt, ...) {
       switch (*fmt) {
         case 'd': 
           {
-            int outv = va_arg(args, int);
+            int outv = va_arg(ap, int);
             // int64_t have 20 digits at most (including neg sign)
             // so 20 Byte buffer is enough
             char outbuf[20] = {0};
             unsigned bufptr = 0;
             if (outv < 0) {
+              if (cnt++ + 1 >= n) { goto vnfinish; }
               *out++ = '-';
               outv = -outv;
             }
@@ -50,14 +75,16 @@ int sprintf(char *out, const char *fmt, ...) {
             } while (outv);
 
             while (bufptr--) {
+              if (cnt++ + 1 >= n) { goto vnfinish; }
               *out++ = outbuf[bufptr];
             }
           }
           break;
         case 's': 
           {
-            const char* outs = va_arg(args, const char*);
+            const char* outs = va_arg(ap, const char*);
             while (*outs != '\0') {
+              if (cnt++ + 1 >= n) { goto vnfinish; }
               *out++ = *outs++;
             }
           }
@@ -69,17 +96,15 @@ int sprintf(char *out, const char *fmt, ...) {
     }
     fmt++;
   }
-  va_end(args);
+
+vnfinish:
   *out++ = '\0';
-  return (int) (out - orig_out);
-}
-
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
-}
-
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  cnt++;
+  if (cnt != (size_t) (out - orig_out)) { 
+    putch(cnt/10+'0'); putch(cnt%10+'0'); putch('\n');
+    assert(0);
+  }; 
+  return cnt;
 }
 
 #endif
