b16ab6f3aaf7e6ce42939e6dc3ca1ad672225b63
tracer-ysyx <tracer@ysyx.org>
2025-11-07 16:38:06 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  16:38:05 up  6:33,  1 user,  load average: 1.14, 1.03, 0.93

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -14,6 +14,7 @@ static uint16_t HEIGHT = 0;
 static uint16_t VM_SIZE = 0;
 
 int
+__attribute_maybe_unused__
 __attribute__((noinline))
 __am_gpu_init_helper(int i) {
   return i + 1;
@@ -25,11 +26,11 @@ void __am_gpu_init() {
   VM_SIZE = WIDTH * HEIGHT * sizeof(uint32_t);
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (uint16_t i = 0; i < 10 + 0* WIDTH * HEIGHT; i ++) {
-    fb[i] = __am_gpu_init_helper(i)-1;
+    fb[i] = i; // __am_gpu_init_helper(i)-1;
   }
 
   outl(SYNC_ADDR, 1);
-  printf("AM WIDTH x HEIGHT = %d x %d\n", WIDTH, HEIGHT);
+  // printf("AM WIDTH x HEIGHT = %d x %d\n", WIDTH, HEIGHT);
   // while (1);
 }
 
