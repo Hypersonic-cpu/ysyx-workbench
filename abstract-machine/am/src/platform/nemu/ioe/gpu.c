6f03de517f332272ddf6e2db5fbba1d689441555
tracer-ysyx <tracer@ysyx.org>
2025-11-07 17:58:29 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  17:58:29 up  7:52,  1 user,  load average: 2.19, 1.86, 1.30

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -57,10 +57,10 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   uint32_t* src_pos = (uint32_t *) (ctl->pixels);
   printf("FBDRAW %d %d\n", ctl->x, ctl->y);
   for (size_t j = 0; j < ctl->h; j++) {
-    // memmove(cur_pos, src_pos, __am_gpu_init_helper(ctl->w)-1);
-    for (size_t kk = 0; kk < ctl->w; kk++) {
-      *(cur_pos + kk) = *(src_pos+kk);
-    }
+    memmove(cur_pos, src_pos, ctl->w * sizeof(uint32_t));
+    // for (size_t kk = 0; kk < ctl->w; kk++) {
+    //   *(cur_pos + kk) = *(src_pos+kk);
+    // }
     cur_pos += WIDTH;
     src_pos += ctl->w;
   }
