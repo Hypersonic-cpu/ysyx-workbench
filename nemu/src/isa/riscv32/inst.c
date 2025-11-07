c2e35754e4c5beb5a1afd035a43b09abdf028102
tracer-ysyx <tracer@ysyx.org>
2025-11-08 00:00:24 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  00:00:24 up 13:05,  1 user,  load average: 1.01, 0.58, 0.57

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -69,8 +69,8 @@ static void frame_trace(vaddr_t jtar, int rd, vaddr_t snpc) {
     frames.stack[sp].ra = snpc;
     frames.stack[sp].sp = R(2);
     frames.stack[sp].symt_idx = idx;
-    // spaces_fmt(sp);
-    // fprintf(stderr, "+Fr[%3d] 0x%8x: %s\n", sp, jtar, symbols.table[idx].name);
+    spaces_fmt(sp);
+    fprintf(stderr, "+Fr[%3d] 0x%8x: %s\n", sp, jtar, symbols.table[idx].name);
   } else {
     // Jump to non-symbol places
     if (rd != 0) {
@@ -93,10 +93,10 @@ static void frame_trace(vaddr_t jtar, int rd, vaddr_t snpc) {
           __attribute_maybe_unused__
           rv32_frame frm = frames.stack[i];
           // printf("-Ret[%3u]\n", i); printf(" frm symt_idx %u\n", frm.symt_idx);
-          // spaces_fmt(i);
-          // fprintf(stderr, "-Fr[%3d] " FMT_WORD ": %s%s\n", 
-          //        i, frm.fn, symbols.table[frm.symt_idx].name,
-          //        (i == retsrc) ? "" : " (TCO skip)");
+          spaces_fmt(i);
+          fprintf(stderr, "-Fr[%3d] " FMT_WORD ": %s%s\n", 
+                 i, frm.fn, symbols.table[frm.symt_idx].name,
+                 (i == retsrc) ? "" : " (TCO skip)");
         }
         // New stack top index is retsrc-1
         frames.num = retsrc;
