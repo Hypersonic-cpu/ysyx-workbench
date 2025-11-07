7d031e87d245bb00f105b3ea4bb67eb0d3b10c63
tracer-ysyx <tracer@ysyx.org>
2025-11-07 15:13:26 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  15:13:26 up  5:17,  1 user,  load average: 0.92, 0.89, 0.76

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -96,6 +96,8 @@ static void checkregs(CPU_state *ref, vaddr_t pc, vaddr_t dnpc) {
   if (!isa_difftest_checkregs(ref, pc, dnpc)) {
     nemu_state.state = NEMU_ABORT;
     nemu_state.halt_pc = pc;
+    IFDEF(CONFIG_ITRACE, void inst_ringbuf_display(););
+    MUXDEF(CONFIG_ITRACE, inst_ringbuf_display(), printf("Inst ring buffer disabled\n"));
     isa_reg_display();
   }
 }
