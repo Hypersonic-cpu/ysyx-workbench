ccf61c11978424d48257531fddbf6815cc410b39
tracer-ysyx <tracer@ysyx.org>
2025-11-07 17:49:57 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  17:49:57 up  7:44,  1 user,  load average: 1.00, 0.87, 0.87

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -24,10 +24,10 @@ void __am_gpu_init() {
   WIDTH  = inw(VGACTL_ADDR + 2);
   HEIGHT = inw(VGACTL_ADDR + 0);
   VM_SIZE = WIDTH * HEIGHT * sizeof(uint32_t);
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (uint16_t i = 0; i < 800 + 0*WIDTH * HEIGHT; i ++) {
-    fb[i] = 0x00aa7755; // __am_gpu_init_helper(i)-1;
-  }
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // for (uint16_t i = 0; i < 800 + 0*WIDTH * HEIGHT; i ++) {
+  //   fb[i] = 0x00aa7755; // __am_gpu_init_helper(i)-1;
+  // }
 
   outl(SYNC_ADDR, 1);
 }
@@ -45,6 +45,10 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
+  }
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (uint16_t i = 0; i < 800 + 0*WIDTH * HEIGHT; i ++) {
+    fb[i] = 0x0000ff00; // __am_gpu_init_helper(i)-1;
   }
   // uint32_t* cur_pos = ((uint32_t *) FB_ADDR) + ctl->y * WIDTH + ctl->x;
   // uint32_t* src_pos = (uint32_t *) (ctl->pixels);
