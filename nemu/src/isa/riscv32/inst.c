989b230d6488b8f89b8f300504064413405dd133
tracer-ysyx <tracer@ysyx.org>
2025-11-08 14:15:16 +0800

    > compile NEMU ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  14:15:16 up 1 day,  3:20,  1 user,  load average: 1.36, 1.20, 1.11

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -35,7 +35,7 @@ enum {
 #define src2R() do { *src2 = R(rs2); } while (0)
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
-#define immS() do { *imm = (SEXT( (BITS(i, 31, 25) << 5) | BITS(i, 11, 7), 12 )); } while(0)
+#define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 #define immJ() do { *imm = \
   (SEXT(BITS(i, 31, 31), 1) << 20) | \
   (BITS(i, 19, 12) << 12) | \
