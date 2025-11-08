b3f11355a133b6ada2fd09a56591b1591fa5972a
tracer-ysyx <tracer@ysyx.org>
2025-11-08 17:10:44 +0800

    > run with AM ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  17:10:44 up 1 day,  6:15,  1 user,  load average: 1.19, 0.97, 0.94

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -1,5 +1,8 @@
 #include <am.h>
+#include <cstdint>
 #include <klib-macros.h>
+
+#include "addrmap.h"
 
 extern char _heap_start;
 int main(const char *args);
@@ -12,6 +15,7 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
 
 void putch(char ch) {
+  *((char*)(uintptr_t)RV32_NPC_SERIAL) = ch;
 }
 
 void halt(int code) {
