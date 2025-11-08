69483ababc83d91aeb04dbe6d27af8cf1828df27
tracer-ysyx <tracer@ysyx.org>
2025-11-08 21:20:03 +0800

    > generate verilog ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 17:47:04 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  21:20:03 up 1 day,  9:28,  1 user,  load average: 0.85, 0.96, 0.99

--- npc/rvproc/sim-cxx/main.cc
+++ npc/rvproc/sim-cxx/main.cc
@@ -20,8 +20,22 @@ single_cycle(
   top->eval();
 }
 
+inline void 
+single_reset(
+    const std::unique_ptr<TOP_NAME>& top, 
+    const std::unique_ptr<VerilatedContext>& context) {
+
+  context->timeInc(1);
+  top->reset = 1;
+  for (size_t i = 0; i < 5; i++) {
+    single_cycle(top, context);
+  }
+  top->reset = 0;
+  single_cycle(top, context);
+}
+
 int 
-main() {
+main(int argc, char* argv[]) {
   const std::unique_ptr<VerilatedContext> contextp { new VerilatedContext };
 
   Verilated::traceEverOn(true);
@@ -32,43 +46,12 @@ main() {
   // Trace 99 levels of hierarchy (or see below)
   top->trace(tfp, 99);
   // tfp->dumpvars(1, "t"); // trace 1 level under "t"
-  //
-  tfp->open("/home/kong/ysyx-workbench/npc/build-sim/rvproc/logs/jalr.log");
-  
-  // int passed = 0;
-  // int failed = 0;
-  //
-  // constexpr vluint64_t TimeMax = 100U;
-  // srand(time(0));
-  // while (contextp->time() < TimeMax && !contextp->gotFinish()) {
-  //   int a = rand() % 15 + 1;
-  //   int b = rand() % 15 + 1;
-  //   top->io_value1 = a;
-  //   top->io_value2 = b;
-  //   top->io_loadingValues = 1;
-  //   single_cycle(top, contextp);
-  //   single_cycle(top, contextp);
-  //   top->io_loadingValues = 0;
-  //   while (!top->io_outputValid && contextp->time() < TimeMax) {
-  //     printf("%lu: a = %d, b = %d, GCD = %d\n", contextp->time(), a, b, top->io_outputGCD);
-  //     single_cycle(top, contextp);
-  //     tfp->dump(contextp->time());
-  //   }
-  //   if (contextp->time() < TimeMax) {
-  //     printf("a = %d, b = %d, GCD = %d\n", a, b, top->io_outputGCD);
-  //     if (top->io_outputGCD == std::gcd(a, b)) { passed ++; }
-  //     else { failed ++; }
-  //   } else {
-  //     printf("a = %d, b = %d, Timeout", a, b);
-  //     failed ++;
-  //   }
-  //   tfp->dump(contextp->time());
-  //   if (passed + failed) { break; }
-  // }
-  //
+  tfp->open("/home/kong/ysyx-workbench/npc/build-sim/rvproc/logs/simcc.log");
 
-  constexpr size_t MaxCyc{ 10U };
-  size_t currCyc{ 0U };
+  single_reset(top, contextp);
+
+  constexpr size_t MaxCyc{ 30U };
+  size_t currCyc{ 1U };
   while (currCyc < MaxCyc) {
     single_cycle(top, contextp);
 
