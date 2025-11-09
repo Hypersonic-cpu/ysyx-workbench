a5409fbcfcab5bfeebee75f1e954f49e91202f52
tracer-ysyx <tracer@ysyx.org>
2025-11-10 00:56:21 +0800

    > generate verilog ysyx_22040000 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  00:56:21 up  1:48,  1 user,  load average: 0.38, 0.52, 0.71

--- npc/rvproc/sim-cxx/main.cc
+++ npc/rvproc/sim-cxx/main.cc
@@ -69,7 +69,7 @@ main(int argc, char* argv[]) {
   }
   for (uint8_t i = 0; i < 16; ++i) {
     auto [v, res] = ccdb::read_reg(top, i);
-    std::cerr << "Reg [" << std::setw(2) << std::dec << i <<
+    std::cerr << "Reg [" << std::dec << std::setw(2)<< i <<
       "] : 0x" << std::hex << std::setw(8) << res << std::endl;
   }
   {
