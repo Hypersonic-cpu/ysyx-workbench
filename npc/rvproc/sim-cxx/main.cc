5fb5582a553ca48aff85855afff73bb3d38355de
tracer-ysyx <tracer@ysyx.org>
2025-11-10 13:40:23 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  13:40:23 up  5:52,  1 user,  load average: 1.02, 1.11, 1.00

--- npc/rvproc/sim-cxx/main.cc
+++ npc/rvproc/sim-cxx/main.cc
@@ -73,17 +73,17 @@ main(int argc, char* argv[]) {
   for (uint16_t i = 0; i < 16; ++i) {
     auto [v, res] = ccdb::read_reg(top, i);
     std::cerr << "Reg [" << std::dec << std::setw(2)<< i << "] : ";
-    cfmt::sout32(std::cerr) << res << std::endl;
+    comm::sout32(std::cerr) << res << std::endl;
   }
   {
     auto [v, res] = ccdb::read_reg(top, 0xff);
     std::cerr << "Reg [PC] : ";
-    cfmt::sout32(std::cerr) << res << std::endl;
+    comm::sout32(std::cerr) << res << std::endl;
   }
   {
     for (uint32_t i = 0; i < 16; i += 4) {
       auto [v, res] = ccdb::read_mem(0x8000'0000U + i);
-    cfmt::sout32(std::cerr, "") << res << " ";
+    comm::sout32(std::cerr, "") << res << " ";
     }
     std::cerr << std::endl;
   }
