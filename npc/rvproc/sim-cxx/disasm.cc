9691ea9198319f4dff6b5506703d5738d7b48329
tracer-ysyx <tracer@ysyx.org>
2025-11-10 21:51:51 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  21:51:51 up  1:37,  1 user,  load average: 1.43, 1.16, 1.00

--- npc/rvproc/sim-cxx/disasm.cc
+++ npc/rvproc/sim-cxx/disasm.cc
@@ -106,9 +106,10 @@ ccdb::init_elfsym(const char *elf_file) {
       assert(comm::elf_syms.find(addr) == comm::elf_syms.end() && 
           "Multiple symbols at the same addr");
       comm::elf_syms.insert_or_assign(addr, 
-          /* Name */ std::string(sym_name), 
+          comm::ElfSymEnt {
+          /* Name */  sym_name, 
           /* Address */ sym_table[i].st_value, 
-          /* Size */ sym_table[i].st_size
+          /* Size */ sym_table[i].st_size }
           );
     }
   }
