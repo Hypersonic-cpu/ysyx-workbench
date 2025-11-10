d76291002dd2d3b4a40eab4d3e1051b623183149
tracer-ysyx <tracer@ysyx.org>
2025-11-10 14:22:10 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  14:22:10 up  6:33,  1 user,  load average: 1.08, 1.04, 1.00

---
+++ npc/rvproc/sim-cxx/ccdb.cc
@@ -0,0 +1,6 @@
+#include "ccdb.hh"
+#include <vector>
+
+namespace ccdb {
+  std::vector<ElfSymEnt> elf_syms {};
+}
--- npc/rvproc/sim-cxx/disasm.cc
+++ npc/rvproc/sim-cxx/disasm.cc
@@ -1,4 +1,18 @@
 #include "disasm.hh"
+#include "ccdb.hh"
+#include "probe.hh"
+
+#include <cassert>
+#include <cstring>
+#include <fcntl.h>
+#include <cstdlib>
+#include <elf.h>
+#include <iostream>
+#include <iterator>
+#include <ostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
 
 void 
 ccdb::init_disasm() {
@@ -35,3 +49,69 @@ ccdb::disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
   cs_free_dl(insn, count);
 }
 
+void
+ccdb::init_elfsym(const char *elf_file) {
+  if (elf_file == NULL) { return; }
+
+  int fd = open(elf_file, O_RDONLY);
+  assert(fd >= 0 && "Elf file open failed");
+
+  struct stat st;
+  int fs_status = fstat(fd, &st);
+  std::cerr << "Elf file " << elf_file << ", size = " <<  st.st_size << std::endl;
+
+  uint8_t* map = (uint8_t *) mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+  assert(map != MAP_FAILED && "Elf mmap failed");
+
+  Elf32_Ehdr* ehdr = (Elf32_Ehdr*) map;
+  assert(memcmp(ehdr->e_ident, ELFMAG, SELFMAG) == 0 && 
+      "Elf header mismatch");
+  assert(ehdr->e_machine == EM_RISCV &&
+         "Elf ISA mismatch, not RV32");
+  assert(ehdr->e_ident[EI_DATA] == ELFDATA2LSB && 
+         "Elf endianess mismatch");
+
+  // Section header
+  Elf32_Shdr* shdr = (Elf32_Shdr*) (map + ehdr->e_shoff);
+  Elf32_Sym* sym_table = NULL;
+  uint8_t* str_table = NULL;
+  unsigned sym_count = 0;
+
+  for (unsigned i = 0; i < ehdr->e_shnum; ++i) {
+    if (shdr[i].sh_type != SHT_SYMTAB) { continue; }
+    sym_table = (Elf32_Sym *) (map + shdr[i].sh_offset);
+    sym_count = shdr[i].sh_size / sizeof(Elf32_Sym);
+    assert(shdr[i].sh_link < ehdr->e_shnum && 
+           "String table out of bound");
+    str_table = map + shdr[shdr[i].sh_link].sh_offset;
+    break;
+  }
+
+  assert(sym_table && "Elf symbol table not found");
+  assert(str_table && "Elf string table not found");
+
+  elf_syms.reserve(sym_count);
+  for (unsigned i = 0; i < sym_count; ++i) {
+    [[maybe_unused]] int bind = ELF32_ST_BIND(sym_table[i].st_info);
+    [[maybe_unused]] int type = ELF32_ST_TYPE(sym_table[i].st_info);
+    const char*
+      sym_name = (const char*) (str_table + sym_table[i].st_name);
+
+    if (type == STT_FUNC) {
+      elf_syms.emplace_back(sym_name, 
+          /* Address */ sym_table[i].st_value, 
+          /* Size */ sym_table[i].st_size);
+    }
+  }
+
+  std::cerr <<  "\n === ELF Funct Symbols (" << elf_syms.size() << " total) === ";
+  std::cerr << std::endl;
+  for (auto const& ent : elf_syms) {
+    comm::sout32(std::cerr) << ent.addr << " size " << std::dec << ent.size;
+    std::cerr << " : " << ent.name << std::endl;
+  }
+
+  munmap(map, st.st_size);
+  close(fd);
+  // TODO: Clear frame stack
+}
