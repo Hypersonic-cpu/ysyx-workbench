1913908297eb1857d89711c2b47d31718b48e3a9
tracer-ysyx <tracer@ysyx.org>
2025-11-10 14:31:14 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  14:31:14 up  6:42,  1 user,  load average: 1.18, 1.01, 0.97

--- npc/rvproc/sim-cxx/ccdb.cc
+++ npc/rvproc/sim-cxx/ccdb.cc
@@ -7,3 +7,24 @@ std::pair<bool, uint32_t>
 ccdb::read_reg(ptop_t top, uint8_t regid) {
   return ccdb::_read_verilator_reg(top, regid);
 }
+
+std::pair<bool, uint32_t>
+ccdb::read_mem(uint32_t addr) {
+  return dpic::pmem_probe(addr);
+}
+
+void 
+ccdb::inst_trace(uint32_t pc) {
+  auto [v, inst] = ccdb::read_mem(pc);
+  assert(v && "ccdb inst read fail");
+
+  constexpr size_t BufferLen{ 256U };
+  char buf[BufferLen] = {0};
+  void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+  disassemble(buf, BufferLen, pc, (uint8_t*) (&inst), 4);
+
+  auto ent = comm::InstEnt{ pc, inst, buf };
+  comm::instBuf.append(ent);
+  ent.printent(std::cerr);
+}
+
