793634f2d54c3f65c0b8d17b505e26086589d82b
tracer-ysyx <tracer@ysyx.org>
2025-11-10 13:39:34 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  13:39:34 up  5:51,  1 user,  load average: 2.17, 1.29, 1.05

---
+++ npc/rvproc/dpic/probe.hh
@@ -0,0 +1,91 @@
+#pragma once
+#include <algorithm>
+#include <cstdint>
+#include <iomanip>
+#include <iostream>
+#include <array>
+
+/** WARN:
+ *  该文件禁止引用 ccdb.{cc,hh} 和 pmemacc.{cc,hh}, 
+ *  而只能被他们引用. 
+ */
+namespace comm {
+  inline std::ostream& 
+  sout32(std::ostream& os, std::string prefix="0x") {
+    os << prefix << std::setfill('0') << std::setw(8) << std::hex;
+    return os;
+  }
+
+  class InstEnt {
+    public:
+      uint32_t const pc;
+      uint32_t const inst;
+      std::string const disasm;
+      void printent(std::ostream& os) const {
+        comm::sout32(os) << pc << " : ";
+        comm::sout32(os, "") << inst << " \t" << disasm;
+        os << std::endl;
+      }
+  };
+
+  class MemEnt {
+    public:
+      uint32_t const addr;
+      bool const is_write;
+      uint32_t const value;
+      // Otherwise ostream<< will treat it as char.
+      uint16_t addr_mask;
+      void printent(std::ostream& os) const {
+        os << (is_write ? "Write" : "Read ");
+        comm::sout32(os, " @ 0x") << addr << " : ";
+        comm::sout32(os, "") << value;
+        if (is_write) { os << " mask " << std::hex << std::setw(1) << addr_mask; }
+        os << std::endl;
+      }
+  };
+  
+  template<class T, std::size_t N>
+  class RingBuffer {
+    public:
+      RingBuffer() : ptr{ 0U }, buf{} {}
+      const T& append(const T& t) {
+        buf.at(ptr).~T();
+        new (&buf.at(ptr)) T(t);
+        auto const& ret = buf.at(ptr);
+        ptr = (ptr + 1) % N;
+        return ret;
+      }
+
+      T const atmod(size_t idx) const {
+        return buf.at(idx % N);
+      }
+
+      T& atmod(size_t idx) {
+        return buf.at(idx % N);
+      }
+
+      void printbuf(std::ostream& os, const std::string& title) const {
+        os << "\n === " << title << " === " << std::endl;
+        for (size_t i = 0; i < N; i++) {
+          atmod(i).printent(os);
+        }
+      }
+
+    protected:
+      size_t ptr;
+      std::array<T, N> buf;
+  };
+
+  /** Global var */
+  extern RingBuffer<InstEnt, 16> instBuf;
+  extern RingBuffer<MemEnt, 16> memBuf;
+
+  void mem_acc_log(
+      uint32_t addr, bool is_write, uint32_t data, uint8_t byte_mask);
+}
+
+// NOTE: 这是main用于窥探dpic SV 的namespace.
+// DPI-C 选择暴露这些接口. 定义应该在 pememacc.cc.
+namespace dpic {
+  std::pair<bool, uint32_t> pmem_probe(uint32_t addr);
+}
