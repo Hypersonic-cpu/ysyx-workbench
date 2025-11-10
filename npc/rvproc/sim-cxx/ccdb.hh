42f7c8251606d6e873576038f0d02b168d11e90d
tracer-ysyx <tracer@ysyx.org>
2025-11-10 13:05:11 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  13:05:11 up  5:16,  1 user,  load average: 1.58, 1.09, 0.89

--- npc/rvproc/sim-cxx/ccdb.hh
+++ npc/rvproc/sim-cxx/ccdb.hh
@@ -124,5 +124,20 @@ namespace ccdb {
 
   void inline trace_init() { init_disasm(); }
 
-  void inst_trace(uint32_t pc);
+  // void inst_trace(uint32_t pc);
+  
+  void 
+  inst_trace(uint32_t pc) {
+    auto [v, inst] = ccdb::read_mem(pc);
+    assert(v && "ccdb inst read fail");
+
+    constexpr size_t BufferLen{ 256U };
+    char buf[BufferLen] = {0};
+    void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+    disassemble(buf, BufferLen, pc, (uint8_t*) (&inst), 4);
+
+    auto ent = InstEnt{ pc, inst, buf };
+    instBuf.append(ent);
+    ent.printent(std::cerr);
+  }
 }
