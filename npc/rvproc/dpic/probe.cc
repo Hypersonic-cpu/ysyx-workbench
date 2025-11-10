9e767d21622159faeb0ba68498f0fb5d9bd3da6d
tracer-ysyx <tracer@ysyx.org>
2025-11-10 14:39:48 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  14:39:48 up  6:51,  1 user,  load average: 0.68, 0.88, 0.94

--- npc/rvproc/dpic/probe.cc
+++ npc/rvproc/dpic/probe.cc
@@ -8,5 +8,7 @@ namespace comm {
 
 void 
 comm::mem_acc_log(uint32_t addr, bool is_write, uint32_t data, uint8_t byte_mask) {
-  memBuf.append(MemEnt{ addr, is_write, data, byte_mask }).printent(std::cerr);
+  memBuf.append(MemEnt{ addr, is_write, data, byte_mask })
+    // .printent(std::cerr)
+    ;
 }
