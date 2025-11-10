5721cd9702cece19c10780a5809d387d66199b7b
Yijiang Kong <KongYijiang@stu.xjtu.edu.cn>
2025-11-10 13:40:54 +0800

    De-couple DPI-C and Verilator main.cc/ccdb
    Create namespace comm:: (dpic/probe.{cc,hh}) that shares global
    variables between DPI-C and sim-cxx/*. So DPI-C does not includes
    sim-cxx/*.hh. Otherwise, ODR violation might occur.

---
+++ npc/rvproc/dpic/probe.cc
@@ -0,0 +1,11 @@
+#include "probe.hh"
+
+namespace comm {
+  RingBuffer<InstEnt, 16> instBuf {}; 
+  RingBuffer<MemEnt, 16> memBuf {};
+}
+
+void 
+comm::mem_acc_log(uint32_t addr, bool is_write, uint32_t data, uint8_t byte_mask) {
+  memBuf.append(MemEnt{ addr, is_write, data, byte_mask }).printent(std::cerr);
+}
