7b29cf74a22e5682a726be0b8b5c2f1bf496e8e0
tracer-ysyx <tracer@ysyx.org>
2025-11-10 13:01:30 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  13:01:30 up  5:13,  1 user,  load average: 0.42, 0.84, 0.80

--- npc/rvproc/dpic/pmemacc.cc
+++ npc/rvproc/dpic/pmemacc.cc
@@ -92,7 +92,7 @@ namespace rv_device {
 
 extern "C" void 
 pmem_init() {
-  // ccdb::pmem_init_hello();
+  ccdb::pmem_init_hello();
 #if PRINTF_COND
   std::cout << "DPI-C >> pmem_init called" << std::endl;
 #endif
