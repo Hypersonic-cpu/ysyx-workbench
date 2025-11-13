42ef655626ec8b2396faeb54b71dd585abaf9a2d
tracer-ysyx <tracer@ysyx.org>
2025-11-13 23:00:48 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  23:00:48 up 8 min,  1 user,  load average: 0.35, 0.49, 0.30

--- npc/rvproc/sim-cxx/main.cc
+++ npc/rvproc/sim-cxx/main.cc
@@ -116,7 +116,7 @@ main(int argc, char* argv[]) {
         comm::sout32(std::cerr) << ref << " got ";
         comm::sout32(std::cerr) << dut << std::endl;
       }
-      ccdb::dump_print(ccdb::DumpPrint{ false, true, true, false, false });
+      ccdb::dump_print(ccdb::DumpPrint{ false, true, true, true, false });
       exit(1);
     }
     currCyc++;
