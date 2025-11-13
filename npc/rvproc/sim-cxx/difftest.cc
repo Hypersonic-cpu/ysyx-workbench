c6bc3f3d6ecfc0594a8c0cd91e94aa704f3ba758
tracer-ysyx <tracer@ysyx.org>
2025-11-13 14:29:44 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  14:29:44 up  1:07,  1 user,  load average: 0.71, 0.85, 0.63

--- npc/rvproc/sim-cxx/difftest.cc
+++ npc/rvproc/sim-cxx/difftest.cc
@@ -68,7 +68,6 @@ diff::copy() {
 
 void 
 diff::iota(uint64_t n) {
-  std::cerr << "==> Id1     " << comm::device_access[comm::PrevCyc] << std::endl;
   if (comm::device_access[comm::PrevCyc]) return;
   ref_exec(n);
 }
