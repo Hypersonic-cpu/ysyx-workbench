81fd1000bb1dc58693a3a75d2b6d29e396f687e2
tracer-ysyx <tracer@ysyx.org>
2025-11-14 20:04:01 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  20:04:01 up 56 min,  1 user,  load average: 1.03, 0.87, 0.90

--- npc/rvproc/src/Elaborate.scala
+++ npc/rvproc/src/Elaborate.scala
@@ -1,39 +1,18 @@
-import chisel3._
-import circt.stage.ChiselStage
-import circt.stage.FirtoolOption // 用于传递firtool选项
-import chisel3.stage.ChiselGeneratorAnnotation // 明确导入 Chisel Generator Annotation
+import scala.util.Properties
 import java.nio.file.Paths
 
 object Elaborate extends App {
-  val npcHome = sys.env.get("NPC_HOME").get
-  val outputDir = Paths.get(npcHome, "build-sv/rvproc/").toString()
 
-  // 1. FIRTOOL 的命令行参数列表 (作为字符串数组)
-  val firtoolOptionsString = Array(
-    // s"-o=$outputDir", // 指定输出目录
-    "--split-verilog", // 启用分文件输出
-    "--top=rvCore", // 保持 Top 模块名称
+  val firtoolOptions = Array(
+    "--split-verilog",
     "--lowering-options=" + List(
+      // make yosys happy
+      // see https://github.com/llvm/circt/blob/main/docs/VerilogGeneration.md
       "disallowLocalVariables",
       "disallowPackedArrays",
       "locationInfoStyle=wrapInAtSquareBracket"
     ).reduce(_ + "," + _)
   )
 
-  // 2. ChiselStage 的基本参数 (指定目标语言)
-  val chiselArgs = Array(
-    "--target", "systemverilog"
-  )
-
-  // 3. 关键：使用 execute API
-  // 注意：在 Chisel 6.x 中，将 firtool 的命令行参数直接放在 ChiselStage.execute 的第一个参数数组中是推荐的做法。
-  (new ChiselStage).execute(
-    // 命令行参数：ChiselStage参数 + firtool参数
-    chiselArgs ++ firtoolOptionsString, 
-    // Annotations：指定要编译的模块
-    Seq(
-      ChiselGeneratorAnnotation(() => new rvproc.rvCore()),
-      FirtoolOption(firtoolOptionsString.mkString(" "))
-    )
-  )
+  circt.stage.ChiselStage.emitSystemVerilogFile(new rvproc.rvCore(), args, firtoolOptions)
 }
