7efac480df8d7a7edd3a2e629af663d2ae50934c
tracer-ysyx <tracer@ysyx.org>
2025-11-14 20:22:41 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  20:22:41 up  1:14,  1 user,  load average: 1.04, 1.00, 0.97

--- npc/rvproc/src/Elaborate.scala
+++ npc/rvproc/src/Elaborate.scala
@@ -8,6 +8,7 @@ object Elaborate extends App {
     "-o", "/home/kong/ysyx-workbench/npc/build-sv/rvproc/",
     "--disable-aggressive-merge-connections",
     "--disable-opt",
+    "--preserve-values=all",
     "--lowering-options=" + List(
       // make yosys happy
       // see https://github.com/llvm/circt/blob/main/docs/VerilogGeneration.md
