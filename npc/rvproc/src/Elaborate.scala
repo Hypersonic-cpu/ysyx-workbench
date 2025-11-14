3af02e809ed8f7d3d9876ff3dbaf746856c823b9
tracer-ysyx <tracer@ysyx.org>
2025-11-14 20:10:25 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  20:10:25 up  1:02,  1 user,  load average: 1.30, 0.98, 0.92

--- npc/rvproc/src/Elaborate.scala
+++ npc/rvproc/src/Elaborate.scala
@@ -6,7 +6,6 @@ object Elaborate extends App {
   val firtoolOptions = Array(
     "--split-verilog",
     "-o", "/home/kong/ysyx-workbench/npc/build-sv/rvproc/",
-    "--top", "rvCore",
     "--lowering-options=" + List(
       // make yosys happy
       // see https://github.com/llvm/circt/blob/main/docs/VerilogGeneration.md
