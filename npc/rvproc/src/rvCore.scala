8332a171cb626a9f285e5267a87426e0a668c5ff
tracer-ysyx <tracer@ysyx.org>
2025-11-14 21:15:41 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  21:15:41 up  2:07,  1 user,  load average: 1.98, 1.25, 1.13

--- npc/rvproc/src/rvCore.scala
+++ npc/rvproc/src/rvCore.scala
@@ -326,6 +326,7 @@ class WBU extends Module {
   })
   val jmp = io.pcJmp.jUncond
   val snpc = io.pc + 4.U
+  printf(cf"\twbsel ${io.wbSel} alu ${io.aluV}%x snpc ${snpc}\n")
   // TODO: Is truncation right on branch ? 
   val dnpc = io.aluV(31, 1) ## 0.U(1.W) 
   io.nxpc := Mux(jmp, io.aluV, snpc)
