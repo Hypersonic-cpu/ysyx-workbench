a1e25408b133029838a010720ede162790dbda71
tracer-ysyx <tracer@ysyx.org>
2025-11-15 00:13:02 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  00:13:02 up  5:05,  1 user,  load average: 1.69, 1.18, 0.99

--- npc/rvproc/src/rvCore.scala
+++ npc/rvproc/src/rvCore.scala
@@ -294,9 +294,9 @@ class EXU extends Module {
     )
   // Add, Sltu, Slt
   val anst = MuxCase(ansc(ISA.RegBits-1, 0), Seq(
-    (io.op === IntAluOp.Sll) -> (src1 << src2(4, 0)),
-    (io.op === IntAluOp.Srr) -> Mux(io.sel.rs2Invert,
-      (src1.asSInt >> src2(4, 0)).asUInt, src1 >> src2(4, 0)),
+    // (io.op === IntAluOp.Sll) -> (src1 << src2(4, 0)),
+    // (io.op === IntAluOp.Srr) -> Mux(io.sel.rs2Invert,
+    //   (src1.asSInt >> src2(4, 0)).asUInt, src1 >> src2(4, 0)),
     (io.op === IntAluOp.And) -> (src1 & src2),
     (io.op === IntAluOp.Or ) -> (src1 | src2),
     (io.op === IntAluOp.Xor) -> (src1 ^ src2),
