cff636882e8d7e1e0a5033a7b8a4425b42339edb
tracer-ysyx <tracer@ysyx.org>
2025-11-14 20:15:38 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  20:15:38 up  1:07,  1 user,  load average: 1.31, 1.08, 0.97

--- npc/rvproc/src/rvCore.scala
+++ npc/rvproc/src/rvCore.scala
@@ -328,7 +328,7 @@ class rvCore() extends Module {
   val io = IO(new Bundle{
     // val regPin  = Input(Tp.RegIdxType())
     // val regPrb  = Output(Tp.RegType())
-    val outPC   = Output(Tp.PCType())
+    // val outPC   = Output(Tp.PCType())
   })
 
   // State
@@ -421,8 +421,8 @@ class rvCore() extends Module {
   // iDebug.io.probeOut := iReg.io.probeOut
   // iDebug.io.probePC  := this.pc
 
-  io.outPC := pc
-  dontTouch(io)
+  // io.outPC := pc
+  // dontTouch(io)
   // dontTouch(iWrite.io)
   // dontTouch(iDec.io)
   // dontTouch(iExe.io)
