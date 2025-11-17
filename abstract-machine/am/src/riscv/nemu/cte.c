07e88691827f3bc79dd51b34ad53114069467415
tracer-ysyx <tracer@ysyx.org>
2025-11-17 22:13:46 +0800

    > compile NEMU ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  22:13:46 up  5:33,  1 user,  load average: 0.36, 0.61, 0.58

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,10 +7,6 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
-  // Out of bound on purpose.
-  // printf("\tmcause\t0x%x\n", (uint32_t) c->mcause);
-  // printf("\tmstatus\t0x%x\n",(uint32_t) c->mstatus);
-  // printf("\tmepc\t0x%x\n", (uint32_t) c->mepc);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
