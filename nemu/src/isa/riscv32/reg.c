78632a269e8d98b65b4ef9575efc722548ed9b8c
tracer-ysyx <tracer@ysyx.org>
2025-11-17 23:11:46 +0800

    > compile NEMU ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  23:11:46 up  6:31,  1 user,  load average: 1.08, 0.65, 0.63

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -76,10 +76,10 @@ void isa_csr_display() {
   const size_t selidx[] = { 
     RISCV_CSR_MTVEC, RISCV_CSR_MSTATUS, RISCV_CSR_MCAUSE, RISCV_CSR_MEPC };
   printf("\n === CSR Display === \n");
-  printf("No.   Name \tValue\n");
+  printf("No.   Name       Value\n");
   for (size_t j = 0; j < sizeof(selidx) / sizeof(size_t); ++j) {
     size_t i = selidx[j];
-    printf("0x%-3lx %4s  " FMT_WORD ": \t%d\n", 
+    printf("0x%-3lx %10s " FMT_WORD ":%d\n", 
            i, csr_name(i), csr(i), csr(i));
   }
 }
