ad047f224e97fb1067a7d14bb11c7ae539a95a66
tracer-ysyx <tracer@ysyx.org>
2025-11-18 15:02:22 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  15:02:22 up  1:30,  1 user,  load average: 0.40, 0.77, 0.88

--- npc/rvproc/sim-cxx/difftest.cc
+++ npc/rvproc/sim-cxx/difftest.cc
@@ -26,7 +26,7 @@ diff::init(const char* so, int port) {
   std::string so_file = (so[0] == '/') ? (std::string(so)) : 
     (std::string(nemu_path) + std::string("/") + std::string(so));
   // TODO: Change to RTLD_LAZY
-  void* dl = dlopen("/home/kong/ysyx-workbench/nemu/build/riscv32-nemu-interpreter-so", RTLD_NOW); 
+  void* dl = dlopen("/home/kong/ysyx-workbench/nemu/build/riscv32-nemu-interpreter-so", RTLD_NOW | RTLD_GLOBAL); 
   comm::v_assert(dl, "DiffTest .so", so_file, "open failed:", dlerror());
   // assert(dl && "DiffTest ref .so open failed");
   
