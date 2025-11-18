c04b25b2fcff2695fb7be3e48ac9aec880504ccd
tracer-ysyx <tracer@ysyx.org>
2025-11-18 10:52:19 +0800

    > compile NEMU ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  10:52:19 up  1:08,  1 user,  load average: 0.51, 0.31, 0.40

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -17,7 +17,9 @@ Context* __am_irq_handle(Context *c) {
       default: ev.event = EVENT_ERROR; break;
     }
 
+    printf("AM Context switch from %x ", (uintptr_t) c);
     c = user_handler(ev, c);
+    printf("to %x \n", (uintptr_t) c);
     assert(c != NULL);
   }
 
