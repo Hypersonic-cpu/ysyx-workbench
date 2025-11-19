abc333d7c835ea3b422285354e1787b28c62d484
tracer-ysyx <tracer@ysyx.org>
2025-11-19 19:45:41 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  19:45:41 up  1:01,  1 user,  load average: 1.38, 0.77, 0.65

--- npc/rvproc/sim-cxx/ccdb.cc
+++ npc/rvproc/sim-cxx/ccdb.cc
@@ -67,12 +67,14 @@ ccdb::inst_trace() {
     ccdb::frame_trace(pc+4, dst, rd == 0);
   }
 
-  // bool is_csr = 
-  //   comm::bits(inst, 6, 2) == 0b11100 &&
-  //   comm::bits(inst, 14, 12) != 0b000;
-  // if (is_csr) { 
-  //   comm::device_access[comm::PrevCyc] = true; 
-  // }
+  bool is_csr = 
+    comm::bits(inst, 6, 2) == 0b11100 &&
+    comm::bits(inst, 14, 12) != 0b000;
+  uint16_t csrid = comm::bits(inst, 31, 20);
+  bool diff_csrs = (csrid == 0xF11 || csrid == 0xF12);
+  if (is_csr && diff_csrs) { 
+    comm::device_access[comm::PrevCyc] = true; 
+  }
 }
 
 std::list<ccdb::FrameEnt> ccdb::frame_stk {};
