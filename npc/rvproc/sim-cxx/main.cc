ea56223cdcb67723334db300d243e4b1b1d7764d
tracer-ysyx <tracer@ysyx.org>
2025-11-25 19:58:46 +0800

    > compile NEMU ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  19:58:46 up  4:12,  1 user,  load average: 1.44, 1.09, 0.77

--- npc/rvproc/sim-cxx/main.cc
+++ npc/rvproc/sim-cxx/main.cc
@@ -116,7 +116,7 @@ main(int argc, char* argv[]) {
   while (!contextp->gotFinish() && currCyc < 10) {
     if (diff::enable) { diff::copy(); }       // Comes before exec
 
-    // if (!comm::fast) { ccdb::inst_trace(); }
+    if (!comm::fast) { ccdb::inst_trace(); }
     single_cycle(top, contextp);
     if (!comm::log_wavefile.empty()) {
       tfp->dump(contextp->time());
