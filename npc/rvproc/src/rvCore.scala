96e934046878674a2700484fbf0a2a6b5775e460
tracer-ysyx <tracer@ysyx.org>
2025-11-25 17:26:58 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  17:26:58 up  2:46,  1 user,  load average: 0.98, 0.65, 0.43

--- npc/rvproc/src/rvCore.scala
+++ npc/rvproc/src/rvCore.scala
@@ -30,22 +30,26 @@ import BitMath._
 class rvCore() extends Module {
   val io = IO(new Bundle{
   })
-  val ifs = Module(new FetchStage)
-  val ids = Module(new DecodeStage)
-  val exs = Module(new ExecuteStage)
-  val lss = Module(new MemoryStage)
-  val wbs = Module(new WrBackStage)
-  val reg = Module(new RegFile)
 
-  BusConnect(ifs.io.out, ids.io.in)
-  BusConnect(ids.io.out, exs.io.in)
-  BusConnect(exs.io.out, lss.io.in)
-  BusConnect(lss.io.out, wbs.io.in)
-  BusConnect(wbs.io.out, ifs.io.in)
+  val ifs = Module(new FetchStage)
+  ifs.io.in := DontCare
+  ifs.io.out := DontCare
+  dontTouch(ifs.io.out)
+  // val ids = Module(new DecodeStage)
+  // val exs = Module(new ExecuteStage)
+  // val lss = Module(new MemoryStage)
+  // val wbs = Module(new WrBackStage)
+  // val reg = Module(new RegFile)
+
+  // BusConnect(ifs.io.out, ids.io.in)
+  // BusConnect(ids.io.out, exs.io.in)
+  // BusConnect(exs.io.out, lss.io.in)
+  // BusConnect(lss.io.out, wbs.io.in)
+  // BusConnect(wbs.io.out, ifs.io.in)
   // always_comb
-  BusConnect(ids.io.toReg, reg.io.fromId, BusType.SingleCyc)
-  BusConnect(reg.io.toId, ids.io.fromReg, BusType.SingleCyc)
-  BusConnect(wbs.io.toReg, reg.io.fromWb, BusType.SingleCyc)
+  // BusConnect(ids.io.toReg, reg.io.fromId, BusType.SingleCyc)
+  // BusConnect(reg.io.toId, ids.io.fromReg, BusType.SingleCyc)
+  // BusConnect(wbs.io.toReg, reg.io.fromWb, BusType.SingleCyc)
 }
 
 class rvCoreWrapper() extends Module {
