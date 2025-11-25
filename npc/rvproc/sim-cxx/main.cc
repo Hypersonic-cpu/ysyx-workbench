08ad317b8db40a2a7d722314ff0f75b5d412b8e9
tracer-ysyx <tracer@ysyx.org>
2025-11-25 21:08:36 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-35-generic #35~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 13:30:46 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  21:08:36 up  5:22,  1 user,  load average: 1.17, 0.90, 0.60

--- npc/rvproc/sim-cxx/main.cc
+++ npc/rvproc/sim-cxx/main.cc
@@ -108,7 +108,7 @@ main(int argc, char* argv[]) {
 
   constexpr size_t MaxCyc{ 30U };
   size_t currCyc{ 1U };
-  while (!contextp->gotFinish() && currCyc < MaxCyc) {
+  while (!contextp->gotFinish()) {
 
   comm::sout32(std::cerr) << top->rootp->
         rvCore__DOT__ifs__DOT__pc << "  <<== PC" << std::endl;
