4db4e225a310b8d9cb4509c2ef96fb8d59409cdd
Yijiang Kong <KongYijiang@stu.xjtu.edu.cn>
2025-11-30 11:25:36 +0800

    Fix DiffTest fail with VGA enabled
    Acquire IOMap* in `device/vga.c` and use `map_read` instead of
    `mmio_read` to avoid unexpected difftest skipping. Caused by
    functional read of vgactrl in NEMU when updating device.

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -33,7 +33,8 @@ static void report_mmio_overlap(const char *name1, paddr_t l1, paddr_t r1,
 }
 
 /* device interface */
-void add_mmio_map(const char *name, paddr_t addr, void *space, uint32_t len, io_callback_t callback) {
+IOMap* 
+add_mmio_map(const char *name, paddr_t addr, void *space, uint32_t len, io_callback_t callback) {
   assert(nr_map < NR_MAP);
   paddr_t left = addr, right = addr + len - 1;
   if (in_pmem(left) || in_pmem(right)) {
@@ -50,7 +51,9 @@ void add_mmio_map(const char *name, paddr_t addr, void *space, uint32_t len, io_
   Log("Add mmio map '%s' at [" FMT_PADDR ", " FMT_PADDR "]",
       maps[nr_map].name, maps[nr_map].low, maps[nr_map].high);
 
+  IOMap* ret = &maps[nr_map];
   nr_map ++;
+  return ret;
 }
 
 /* bus interface */
