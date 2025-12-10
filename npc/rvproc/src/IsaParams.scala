850cab1dfc64cb12f5806dce11dd6ea07000969b
Yijiang Kong <KongYijiang@stu.xjtu.edu.cn>
2025-12-10 20:55:44 +0800

    npc: commit by Makefile script

--- npc/rvproc/src/IsaParams.scala
+++ npc/rvproc/src/IsaParams.scala
@@ -10,21 +10,22 @@ object PATH {
 }
 
 object ISA {
-  val InstBits    = 32
-  val RegBits     = 32
-  val RegNum      = 16
-  val RegIdxBits  =  4
-  val CsrIdxBits  = 12
-  val AddrBits    = 32
+  val InstBits   = 32
+  val RegBits    = 32
+  val RegNum     = 16
+  val RegIdxBits = 4
+  val CsrIdxBits = 12
+  val AddrBits   = 32 // Also bus bits
 }
 
 object Tp {
-  def RegType() = UInt(ISA.RegBits.W)
-  def InstType() = UInt(ISA.InstBits.W)
-  def RegIdxType() = UInt(ISA.RegIdxBits.W)
-  def CsrIdxType() = UInt(ISA.CsrIdxBits.W)
+  def RegType()     = UInt(ISA.RegBits.W)
+  def InstType()    = UInt(ISA.InstBits.W)
+  def RegIdxType()  = UInt(ISA.RegIdxBits.W)
+  def CsrIdxType()  = UInt(ISA.CsrIdxBits.W)
   // Now it equals RegType() so no padding is needed.
-  def AddrType() = UInt(ISA.AddrBits.W)
+  def AddrType()    = UInt(ISA.AddrBits.W)
+  def AddrAligner() = ~((ISA.AddrBits / 8 - 1).U(ISA.AddrBits.W))
 }
 
 object ITYPE extends ChiselEnum {
--- npc/rvproc/src/PMemBox.scala
+++ npc/rvproc/src/PMemBox.scala
@@ -3,33 +3,11 @@ package rvproc
 import chisel3._
 import chisel3.util._
 import chisel3.util.HasBlackBoxPath
+import rvproc.axi4.AXILite
 
-/** +-----+ reqValid           ---> +-----+
-  * |     | addr[log2(N)-1:0]  ---> |     |
-  * |     | wrEn|memEn         ---> |     |
-  * | CPU | wdata[31:0]        ---> | MEM |
-  * |     | wmask[3:0]         ---> |     |
-  * |     | <---          respValid |     |- reset
-  * |     | <---        rdata[31:0] |     <- clock
-  * +-----+                         +-----+
-  */
-
-class PMemBox extends BlackBox with HasBlackBoxPath {
-  // Input: Master -> Slave
-  //        Master <- Slave : Output
-  val io = IO(new Bundle {
-    val clock     = Input(Clock())
-    val reset     = Input(Reset())
-    val reqValid  = Input(Bool())
-    val respReady = Input(Bool())
-    val addr      = Input(Tp.AddrType())
-    val wrEn      = Input(Bool())
-    val wrData    = Input(Tp.RegType())
-    val byteMask  = Input(UInt(8.W)) // UInt4
-    val reqReady  = Output(Bool())
-    val respValid = Output(Bool())
-    val respData  = Output(Tp.RegType())
-  })
-
-  addPath(PATH.dpic("PMemBox.sv"))
+class PMemBox extends Module {
+  val io = IO(Flipped(new AXILite))
+  io := DontCare
+  dontTouch(io)
 }
+
