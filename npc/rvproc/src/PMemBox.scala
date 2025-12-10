bca2514c76fd109ac2f2151170ef340c0c23363a
Yijiang Kong <KongYijiang@stu.xjtu.edu.cn>
2025-12-10 23:25:07 +0800

    AXI4-Lite, pass cpu-tests

--- npc/rvproc/src/PMemBox.scala
+++ npc/rvproc/src/PMemBox.scala
@@ -6,8 +6,13 @@ import chisel3.util.HasBlackBoxPath
 import rvproc.axi4.AXILite
 
 class PMemBox extends Module {
-  val io = IO(Flipped(new AXILite))
+  val io = IO(new Bundle {
+    val master = Flipped(new AXILite)
+  })
+  // val fakeReg = Reg(Flipped(new AXILite))
+  // fakeReg := io.master;
   io := DontCare
   dontTouch(io)
+  dontTouch(clock)
+  dontTouch(reset)
 }
-
