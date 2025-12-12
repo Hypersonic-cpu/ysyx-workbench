59ebf3199060524020a26c4c843c0f8d7814248b
Yijiang Kong <KongYijiang@stu.xjtu.edu.cn>
2025-12-12 23:31:41 +0800

    npc: commit by Makefile script

--- npc/rvproc/src/IsaParams.scala
+++ npc/rvproc/src/IsaParams.scala
@@ -26,6 +26,7 @@ object Tp {
   // Now it equals RegType() so no padding is needed.
   def AddrType()    = UInt(ISA.AddrBits.W)
   def AddrAligner() = ~((ISA.AddrBits / 8 - 1).U(ISA.AddrBits.W))
+  def TimeType()    = UInt(64.W)
 }
 
 object ITYPE extends ChiselEnum {
