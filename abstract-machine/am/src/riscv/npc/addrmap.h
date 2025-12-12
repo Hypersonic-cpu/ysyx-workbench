e79be29a5eb60bcc5c19dc7a3d5c3fbd99247690
Yijiang Kong <KongYijiang@stu.xjtu.edu.cn>
2025-12-13 00:27:45 +0800

    Impl CLINT with mtime (npc&am), passing amtest 't'

--- abstract-machine/am/src/riscv/npc/addrmap.h
+++ abstract-machine/am/src/riscv/npc/addrmap.h
@@ -2,6 +2,8 @@
 #define __RISCV_NPC_ADDRMAP_H__
 
 #define RV32_NPC_SERIAL  0x10000000
-#define RV32_NPC_CLOCK   0x10000020
+#define RV32_NPC_CLOCK   0x0200bff8
+// #define NPC_CYC_PER_US   500
+#define NPC_CYC_PER_US   16 // 6.25MHz
 
 #endif // !__RISCV_NPC_ADDRMAP_H__
---
+++ npc/rvproc/src/CLINT.scala
@@ -0,0 +1,59 @@
+package rvproc.device
+
+import chisel3._
+import chisel3.util._
+
+import rvproc._
+import rvproc.BitMath._
+import rvproc.axi4._
+import rvproc.axi4.AXIRespStatus._
+
+object CLINTAddr {
+  val Base   = 0x0200_0000L
+  val Size   = 0xc000L
+  val Rsv    = 0xbfffL
+  val OffClk = 0xbff8L
+}
+
+class CLINT extends Module {
+  val io                  = IO(new Bundle {
+    val port = Flipped(new AXILite)
+  })
+  val idle :: proc :: Nil = Enum(2)
+
+  val state = RegInit(idle)
+  state  := MuxLookup(state, idle)(
+    Seq(
+      idle -> Mux(io.port.ar.valid, proc, idle),
+      proc -> Mux(io.port.r.ready, idle, proc)
+    )
+  )
+  val retExt = RegInit(UInt((ISA.RegBits + 1).W), 0.U)
+
+  io.port.aw.ready    := false.B
+  io.port.w.ready     := false.B
+  io.port.b.valid     := false.B
+  io.port.b.bits.resp := SLVERR
+  assert(!reset.asBool || io.port.aw.valid, "Attemping to write CLINT timer")
+  io.port.ar.ready    := state === idle
+  io.port.r.valid     := state === proc
+  io.port.r.bits.data := retExt >> 1.U
+  io.port.r.bits.resp := Mux(retExt(0), OKAY, SLVERR)
+
+  val mtime = RegInit(Tp.TimeType(), 0xff000000L.U)
+  mtime := mtime + 1.U
+
+  when(io.port.ar.valid) {
+    val addr   = io.port.ar.bits.addr - CLINTAddr.Base.U
+    val gotRet = MuxCase(
+      0xbadc0de.U ## 0.U,
+      Seq(
+        (addr === CLINTAddr.OffClk.U)       -> mtime(31, 0) ## 1.U,
+        (addr === (CLINTAddr.OffClk + 4).U) -> mtime(63, 32) ## 1.U
+      )
+    )
+    // assert(addr(3, 0)===0xc.U, cf"CLINT DEBUG: mt${mtime}%x addr${addr}%x ret${gotRet >> 1.U}%x\n")
+    retExt := gotRet
+    assert(gotRet(0), cf"Unmapped CLINT addr ${addr}%x")
+  }
+}
