ca8dac48568a0ccadfb9975f5fc7ceac8033e7cc
Yijiang Kong <KongYijiang@stu.xjtu.edu.cn>
2025-12-13 17:02:13 +0800

    npc: commit by Makefile script

---
+++ npc/rvproc/src/device/PMemBox.scala
@@ -0,0 +1,18 @@
+package rvproc
+
+import chisel3._
+import chisel3.util._
+import chisel3.util.HasBlackBoxPath
+import rvproc.axi4.AXIBus
+
+class PMemBox extends Module {
+  val io = IO(new Bundle {
+    val master = Flipped(new AXIBus)
+  })
+  // val fakeReg = Reg(Flipped(new AXIBus))
+  // fakeReg := io.master;
+  io := DontCare
+  dontTouch(io)
+  dontTouch(clock)
+  dontTouch(reset)
+}
---
+++ npc/rvproc/src/device/UART.scala
@@ -0,0 +1,51 @@
+package rvproc.device
+
+import chisel3._
+import chisel3.util._
+import chisel3.util.HasBlackBoxPath
+
+import rvproc._
+import rvproc.axi4._
+import rvproc.BitMath._
+import rvproc.axi4.AXI.RespStatus._
+
+class UART extends Module {
+  val io      = IO(new Bundle {
+    val port = Flipped(new AXIBus)
+  })
+  val uartbox = Module(new UARTBox)
+  uartbox.io.clock := clock
+  uartbox.io.reset := reset
+
+  io.port.ar.ready    := false.B
+  io.port.r.valid     := false.B
+  io.port.r.bits.resp := SLVERR
+  io.port.r.bits.data := 0xbadc0de.U
+  assert(~io.port.ar.valid, "AR valid to UART: UnImpl")
+
+  io.port.aw.ready    := true.B
+  io.port.w.ready     := true.B
+  io.port.b.valid     := true.B
+  io.port.b.bits.resp := OKAY
+
+  val lastAw = RegInit(false.B)
+  lastAw := io.port.aw.valid
+  val activate = io.port.aw.valid && !lastAw
+  uartbox.io.bytein := io.port.w.bits.data(7, 0)
+  assert(
+    io.port.w.valid Implies (io.port.w.bits.strb === 1.U),
+    "Serial write: bytemask not 1"
+  )
+  uartbox.io.active := activate
+}
+
+class UARTBox extends BlackBox with HasBlackBoxPath {
+  val io = IO(new Bundle {
+    val clock  = Input(Clock())
+    val reset  = Input(Reset())
+    val bytein = Input(UInt(8.W))
+    val active = Input(Bool())
+  })
+
+  addPath(PATH.dpic("UARTBox.sv"))
+}
