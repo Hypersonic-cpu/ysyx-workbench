be53d4cc4837ce7e4dadb83a8525112a86a8e870
Yijiang Kong <KongYijiang@stu.xjtu.edu.cn>
2025-12-19 15:25:21 +0800

    IF from Flash. Pass CPU-tests
    Fixme: Simulation is realy slow.

--- npc/rvproc/sim-cxx/runtime.cc
+++ npc/rvproc/sim-cxx/runtime.cc
@@ -14,4 +14,7 @@ mrom_read(int32_t addr, int32_t* data) {
 void
 flash_read(int32_t addr, int32_t* data) {
   *(uint32_t*)data = flash->readAligned(addr);
+  // std::cerr << std::hex;
+  // std::cerr << "DPI-C flash read @ " << addr << " data = " << *data
+  //           << std::endl;
 }
