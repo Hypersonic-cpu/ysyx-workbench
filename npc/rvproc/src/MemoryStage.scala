f46c5b0ffa2cb9c48dd1d6d6616cf09cbd5b7b61
Yijiang Kong <KongYijiang@stu.xjtu.edu.cn>
2025-12-25 17:53:22 +0800

    Upd linker.ld

--- npc/rvproc/src/MemoryStage.scala
+++ npc/rvproc/src/MemoryStage.scala
@@ -65,7 +65,7 @@ class MemoryStage extends Module {
   dMem.aw.bits.size  := ioex.memOp.len.asUInt
   dMem.ar.bits.id    := 1.U
   dMem.aw.bits.id    := 1.U
-  dMem.ar.bits.len   := 0.U
+  dMem.ar.bits.len   := 0.U // NOTE: 传输的次数. 大小是 size
   dMem.aw.bits.len   := 0.U
   dMem.w.bits.last   := true.B
 
