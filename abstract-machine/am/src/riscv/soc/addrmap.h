7314357ecdc4e13e905bcc46d7ca3e5d8ec9858a
Yijiang Kong <KongYijiang@stu.xjtu.edu.cn>
2025-12-28 15:12:19 +0800

    AM runtime for PS/2 and UART. Move stack to SRAM

--- abstract-machine/am/src/riscv/soc/addrmap.h
+++ abstract-machine/am/src/riscv/soc/addrmap.h
@@ -1,19 +1,21 @@
 #ifndef __RISCV_SOC_ADDRMAP_H__
 #define __RISCV_SOC_ADDRMAP_H__
 
-// CLINT         0x0200_0000 ~ 0x0200_ffff
-// SRAM          0x0f00_0000 ~ 0x0fff_ffff
-// UART16550     0x1000_0000 ~ 0x1000_0fff
-// SPI master    0x1000_1000 ~ 0x1000_1fff
-// GPIO          0x1000_2000 ~ 0x1000_200f
-// PS2           0x1001_1000 ~ 0x1001_1007
-// MROM          0x2000_0000 ~ 0x2000_0fff
-// VGA           0x2100_0000 ~ 0x211f_ffff
-// Flash         0x3000_0000 ~ 0x3fff_ffff
-// ChipLink MMIO 0x4000_0000 ~ 0x7fff_ffff
-// PSRAM         0x8000_0000 ~ 0x9fff_ffff
-// SDRAM         0xa000_0000 ~ 0xbfff_ffff
-// ChipLink MEM  0xc000_0000 ~ 0xffff_ffff
+/** MMIO
+ * CLINT         0x0200_0000 ~ 0x0200_ffff
+ * SRAM          0x0f00_0000 ~ 0x0fff_ffff
+ * UART16550     0x1000_0000 ~ 0x1000_0fff
+ * SPI master    0x1000_1000 ~ 0x1000_1fff
+ * GPIO          0x1000_2000 ~ 0x1000_200f
+ * PS2           0x1001_1000 ~ 0x1001_1007
+ * MROM          0x2000_0000 ~ 0x2000_0fff
+ * VGA           0x2100_0000 ~ 0x211f_ffff
+ * Flash         0x3000_0000 ~ 0x3fff_ffff
+ * ChipLink MMIO 0x4000_0000 ~ 0x7fff_ffff
+ * PSRAM         0x8000_0000 ~ 0x9fff_ffff
+ * SDRAM         0xa000_0000 ~ 0xbfff_ffff
+ * ChipLink MEM  0xc000_0000 ~ 0xffff_ffff
+ */
 
 #define RV32_SOC_UART_L   0x10000000U
 #define RV32_SOC_CLOCK    0x0200bff8U
@@ -21,7 +23,9 @@
 #define RV32_SOC_MROM_H   0x20001000U
 #define RV32_SOC_SDRAM_L  0xa0000000U
 #define RV32_SOC_SDRAM_H  0xc0000000U
+#define RV32_SOC_PS2      0x10011000U
 
+#define UART_OFF_RTX      0U
 #define UART_OFF_DIV      0U
 #define UART_OFF_THR      0U // Transmit holding reg
 #define UART_OFF_LCR      3U // Line control register
