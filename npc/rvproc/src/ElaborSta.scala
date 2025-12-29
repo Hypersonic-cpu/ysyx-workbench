63acb13c80a7c4bfa3d5328ec3e21de186c43caa
Yijiang Kong <KongYijiang@stu.xjtu.edu.cn>
2025-12-30 02:23:17 +0800

    Module for fast sta

---
+++ npc/rvproc/src/ElaborSta.scala
@@ -0,0 +1,19 @@
+import scala.util.Properties
+import java.nio.file.Paths
+
+object ElaborSta extends App {
+val outputPath = "/home/kong/ysyx-workbench/npc/build-sv/rvproc/mcRvCore.sv"
+  val firtoolOptions = Array(
+    "-o", outputPath,
+    "--lowering-options=" + List(
+      // make yosys happy
+      // see https://github.com/llvm/circt/blob/main/docs/VerilogGeneration.md
+      "disallowLocalVariables",
+      "disallowPackedArrays",
+      "locationInfoStyle=wrapInAtSquareBracket",
+      "emittedLineLength=72",
+    ).reduce(_ + "," + _)
+  )
+
+  circt.stage.ChiselStage.emitSystemVerilogFile(new rvproc.rvCore(), args, firtoolOptions)
+}
