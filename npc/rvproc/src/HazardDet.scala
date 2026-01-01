b51e49e19a4cb2a2732764753198abb8ddcd980e
Yijiang Kong <KongYijiang@stu.xjtu.edu.cn>
2026-01-02 00:12:17 +0800

    Resolve RAW using Flushing
    TODO: cpu-tests failed. Maybe with Load-Store ?

--- npc/rvproc/src/HazardDet.scala
+++ npc/rvproc/src/HazardDet.scala
@@ -10,6 +10,36 @@ import rvproc.pmu.FetchPMU
 import BitMath._
 
 class HazardDet extends Module {
+  val io = IO(new Bundle {})
+}
+
+class RAWDet extends Module {
   val io = IO(new Bundle {
+    val raw    = Output(Bool())
+    val decode = Flipped(Decoupled(new DecodeHazard))
+    val exsrd  = Flipped(Decoupled(Tp.RegIdxType()))
+    val lssrd  = Flipped(Decoupled(Tp.RegIdxType()))
+    val wbsrd  = Flipped(Decoupled(Tp.RegIdxType()))
   })
+
+  io.decode.ready := true.B
+  io.exsrd.ready  := true.B
+  io.lssrd.ready  := true.B
+  io.wbsrd.ready  := true.B
+
+  def conflictWith[T <: DecoupledIO[Data]](
+    self:  DecoupledIO[DecodeHazard],
+    other: T
+  ) = {
+    other.valid && self.valid &&
+    (other.bits === self.bits.rs1 || other.bits === self.bits.rs2)
+  }
+  io.raw :=
+    conflictWith(
+      io.decode,
+      io.exsrd
+    ) || conflictWith(
+      io.decode,
+      io.lssrd
+    ) || conflictWith(io.decode, io.wbsrd)
 }
