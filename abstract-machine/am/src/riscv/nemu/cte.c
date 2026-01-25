d77f65c807268521a64c96abef3bf4340888a4e3
tracer-ysyx <tracer@ysyx.org>
2026-01-25 23:14:31 +0800

    > compile NEMU ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 21 03:10:52 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  23:14:31 up 4 days,  3:47,  3 users,  load average: 0.11, 0.14, 0.14

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -10,8 +10,8 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case 11: 
-        ev.event = EVENT_YIELD; 
+      case 11:
+        ev.event = EVENT_YIELD;
         c->mepc += 4;
         break;
       default: ev.event = EVENT_ERROR; break;
@@ -63,4 +63,14 @@ bool ienabled() {
 }
 
 void iset(bool enable) {
+}
+
+void cc_reset_stats() {
+  asm volatile("li x15, 0x0" ::: "x15", "memory");
+  asm volatile("ebreak");
+}
+
+void cc_dump_stats() {
+  asm volatile("li x15, 0x1" ::: "x15", "memory");
+  asm volatile("ebreak");
 }
