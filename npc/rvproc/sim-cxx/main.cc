fe6f8298de3a81e63c1db6ba07a48030fdc927cb
tracer-ysyx <tracer@ysyx.org>
2026-02-08 13:51:10 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 21 03:10:52 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  13:51:10 up  2:13,  1 user,  load average: 0.13, 0.39, 0.40

--- npc/rvproc/sim-cxx/main.cc
+++ npc/rvproc/sim-cxx/main.cc
@@ -119,15 +119,14 @@ single_cycle(const std::unique_ptr<TOP_NAME>& top,
              const std::unique_ptr<VerilatedContext>& context,
              const std::vector<ClockedObject*>& objlist,
              const trace::FstTracer& wave) {
-  top->clock = 1;
-  top->eval();
-
   for (auto ptr : objlist) {
     ptr->do_update();
   }
   std::cerr << std::format("iCache Avail = {:d} {:d}\n",
                            iCache->is_ready().first,
                            iCache->is_ready().second);
+  top->clock = 1;
+  top->eval();
   wave.dump(context->time());
   context->timeInc(1);
 
