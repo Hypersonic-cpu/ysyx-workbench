8cff247b65aec89b673ab32752d2e3b909cd71af
tracer-ysyx <tracer@ysyx.org>
2026-02-08 13:27:47 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 21 03:10:52 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  13:27:47 up  1:49,  1 user,  load average: 0.68, 0.76, 0.56

--- npc/rvproc/sim-cxx/main.cc
+++ npc/rvproc/sim-cxx/main.cc
@@ -122,10 +122,9 @@ single_cycle(const std::unique_ptr<TOP_NAME>& top,
   for (auto ptr : objlist) {
     ptr->do_update();
   }
-  // std::cerr << std::format("iCache Avail = {:d} {:d}\n",
-  //                          iCache->is_ready().first,
-  //                          iCache->is_ready().second);
-  //
+  std::cerr << std::format("iCache Avail = {:d} {:d}\n",
+                           iCache->is_ready().first,
+                           iCache->is_ready().second);
   top->clock = 1;
   top->eval();
   wave.dump(context->time());
