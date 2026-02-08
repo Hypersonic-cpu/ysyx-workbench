669b0d6b0d909709ab5d3d339c657e83d4aa3831
tracer-ysyx <tracer@ysyx.org>
2026-02-08 15:26:32 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 21 03:10:52 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  15:26:32 up  3:48,  1 user,  load average: 0.37, 0.43, 0.45

--- npc/rvproc/sim-cxx/pmu.hh
+++ npc/rvproc/sim-cxx/pmu.hh
@@ -129,6 +129,7 @@ public:
 
   void
   notifyIFRecvd(addr_t pc) {
+    return;
     std::cerr << std::format("-- ifetch queue search {:x} in [", pc);
     for (auto elem: ifetchboard) {
       std::cerr << std::format("{:x}, ", std::get<0>(elem));
