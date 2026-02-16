5b6444696db14947565bddf08f8dee947efd4a05
tracer-ysyx <tracer@ysyx.org>
2026-02-16 15:39:28 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 21 03:10:52 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  15:39:28 up 16:46,  3 users,  load average: 0.57, 0.35, 0.30

--- npc/rvproc/src/IsaParams.scala
+++ npc/rvproc/src/IsaParams.scala
@@ -23,6 +23,7 @@ object ISA {
   val RegIdxBits = 4
   val CsrIdxBits = 12
   val AddrBits   = 32 // Also bus bits
+  val WordBytes  = 4
 }
 
 object Tp {
--- npc/rvproc/src/cache/iCache.scala
+++ npc/rvproc/src/cache/iCache.scala
@@ -15,6 +15,7 @@ import rvproc.GlbCtrl.debug
 
 case class iCacheConf(
   addrBits:  Int = 32,
+  wordSize:  Int = 32,
   dataBytes: Int = 1024,
   lineBytes: Int = 16,
   assoc: Int = 1) {
@@ -28,6 +29,7 @@ case class iCacheConf(
   def tagBitLo  = addrBits - tagBits
   def lineTrans = this.lineBytes / (this.addrBits / 8)
   def lineTBits = log2Ceil(this.lineTrans)
+  def wordShift = log2Ceil(this.wordSize)
 }
 
 // Readonly
@@ -102,7 +104,7 @@ class iCache(conf: iCacheConf) extends Module {
   val lineRead  = dataArr.read(reqA1, willShift && reqV1)
   val lineSplit =
     VecInit.tabulate(conf.lineBytes)(i => lineRead(i * 4 + 3, i * 4))
-  wordSel := lineSplit(offOf(reqA2))
+  wordSel := lineSplit(offOf(reqA2) >> conf.wordShift.U)
 
   // Cycle 3 (resp)
   val fillFinish = RegNext(io.memSide.r.bits.last && io.memSide.r.valid)
@@ -164,7 +166,7 @@ class iCache(conf: iCacheConf) extends Module {
     tagArr.write(idxOf(reqA2), tagOf(reqA2))
     validArr(idxOf(reqA2)) := true.B
     missServe              := true.B
-    missData               := fillBuf(offOf(reqA2))
+    missData               := fillBuf(offOf(reqA2) >> conf.wordShift.U)
     assert(!resp.fire, "Transaction (resp) during fill\n")
   }.elsewhen(resp.fire) {
     missServe := false.B
