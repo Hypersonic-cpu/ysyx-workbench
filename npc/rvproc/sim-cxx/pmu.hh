1713cb598554943a7487d3c6ce2bf9dff3fde18f
tracer-ysyx <tracer@ysyx.org>
2026-02-16 16:38:04 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 21 03:10:52 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  16:38:04 up 17:44,  3 users,  load average: 0.17, 0.43, 0.42

--- npc/rvproc/sim-cxx/pmu.hh
+++ npc/rvproc/sim-cxx/pmu.hh
@@ -129,6 +129,8 @@ public:
 
   void
   notifyIFRecvd(addr_t pc) {
+    v_assert(!ifetchboard.empty(), "Cannot find pc @", pc,
+             "in IF Pipeline");
     while (std::get<0>(ifetchboard.front()) != pc) {
       ifetchboard.pop_front();
       v_assert(!ifetchboard.empty(), "Cannot find pc @", pc,
