0f143c9baee68d2602f260130a3d479829379de4
tracer-ysyx <tracer@ysyx.org>
2026-02-18 15:33:21 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 21 03:10:52 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  15:33:21 up 2 days, 16:40,  3 users,  load average: 0.68, 0.86, 0.64

--- npc/rvproc/sim-cxx/pmu.hh
+++ npc/rvproc/sim-cxx/pmu.hh
@@ -5,6 +5,7 @@
 #include "stats_template/stats.hpp"
 
 #include <cassert>
+#include <cstdio>
 #include <format>
 #include <iostream>
 #include <limits>
@@ -227,6 +228,7 @@ public:
 
   void
   notifyCacheResp(addr_t addr, bool is_hit, uint16_t id) {
+    std::cerr << std::format("Resp @{:08x} Hit {:d}\n", addr, is_hit);
     assert(id == 0);
     auto& front = icacheboard.front();
     auto [f_addr, f_tick] = front;
@@ -244,6 +246,7 @@ public:
   void
   notifyCacheReq(addr_t addr, uint16_t id) {
     assert(id == 0);
+    std::cerr << std::format("Req @{:08x}\n", addr);
     icacheboard.emplace_back(addr, curr_tick());
   }
 
