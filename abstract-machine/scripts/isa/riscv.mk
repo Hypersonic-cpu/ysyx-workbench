2b426c17ebc16d8775540c5ac4240354c775bca5
tracer-ysyx <tracer@ysyx.org>
2026-02-23 12:47:24 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 21 03:10:52 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  12:47:24 up 2 days,  1:13,  2 users,  load average: 0.48, 0.19, 0.06

--- abstract-machine/scripts/isa/riscv.mk
+++ abstract-machine/scripts/isa/riscv.mk
@@ -1,8 +1,10 @@
 CROSS_COMPILE := riscv64-linux-gnu-
 COMMON_CFLAGS := -fno-pic -march=rv64g -mcmodel=medany -mstrict-align
 CFLAGS        += $(COMMON_CFLAGS) -static
-ASFLAGS       += $(COMMON_CFLAGS) -O2
+ASFLAGS       += $(COMMON_CFLAGS) -O0
 LDFLAGS       += -melf64lriscv
+
+CFLAGS        += -O2
 
 # overwrite ARCH_H defined in $(AM_HOME)/Makefile
 ARCH_H := arch/riscv.h
