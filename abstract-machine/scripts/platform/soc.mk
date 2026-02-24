4c76eb9504b8d277faa578deb3463fcfc5b48193
tracer-ysyx <tracer@ysyx.org>
2026-02-24 19:41:54 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 21 03:10:52 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  19:41:54 up 3 days,  6:41,  4 users,  load average: 0.20, 0.22, 0.20

--- abstract-machine/scripts/platform/soc.mk
+++ abstract-machine/scripts/platform/soc.mk
@@ -17,6 +17,8 @@ MAINARGS_MAX_LEN = 64
 MAINARGS_PLACEHOLDER = the_insert-arg_rule_in_Makefile_will_insert_mainargs_here
 CFLAGS += -DMAINARGS_MAX_LEN=$(MAINARGS_MAX_LEN) -DMAINARGS_PLACEHOLDER=$(MAINARGS_PLACEHOLDER)
 
+SOCMODE := 1
+
 insert-arg: image
 	@python3 $(AM_HOME)/tools/insert-arg.py $(IMAGE).bin $(MAINARGS_MAX_LEN) $(MAINARGS_PLACEHOLDER) "$(mainargs)"
 
