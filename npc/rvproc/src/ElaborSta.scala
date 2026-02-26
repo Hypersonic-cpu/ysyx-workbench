40e73caa39225ad33ab8ef543bc6abd606cbc0b9
tracer-ysyx <tracer@ysyx.org>
2026-02-26 23:38:03 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 21 03:10:52 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  23:38:03 up  7:55,  3 users,  load average: 0.65, 0.35, 0.23

--- npc/rvproc/src/ElaborSta.scala
+++ npc/rvproc/src/ElaborSta.scala
@@ -1,9 +1,43 @@
 import scala.util.Properties
 import java.nio.file.Paths
 import rvproc.cache.iCacheConf
+import rvproc.GlbCtrl
 
 object ElaborSta extends App {
-val outputPath = "/home/kong/ysyx-workbench/npc/build-sv/rvproc/mcRvCore.sv"
+  // Defaults for STA: debug=false, sta=true
+  GlbCtrl.debug = false
+  GlbCtrl.sta = true
+  GlbCtrl.sramlib = false
+
+  var l1iSize    = 512
+  var l1iBlksize = 16
+  var l1iAssoc   = 1
+  val rest = scala.collection.mutable.ArrayBuffer[String]()
+
+  var i = 0
+  while (i < args.length) {
+    args(i) match {
+      case "--l1i-size"    => l1iSize = args(i + 1).toInt; i += 1
+      case "--l1i-blksize" => l1iBlksize = args(i + 1).toInt; i += 1
+      case "--l1i-assoc"   => l1iAssoc = args(i + 1).toInt; i += 1
+      case "--debug"       => GlbCtrl.debug = true
+      case "--no-debug"    => GlbCtrl.debug = false
+      case "--sta"         => GlbCtrl.sta = true
+      case "--no-sta"      => GlbCtrl.sta = false
+      case "--sramlib"     => GlbCtrl.sramlib = true
+      case "--no-sramlib"  => GlbCtrl.sramlib = false
+      case other           => rest += other
+    }
+    i += 1
+  }
+
+  val l1iConf = iCacheConf(32, l1iSize, l1iBlksize, l1iAssoc)
+  l1iConf.printConf()
+
+  val ysyxNPC = System.getenv("NPC_HOME")
+  assert(ysyxNPC != null && ysyxNPC.nonEmpty)
+  val outputPath =
+    ysyxNPC + "/build-sv/rvproc/mcRvCore.sv"
   val firtoolOptions = Array(
     "-o", outputPath,
     "--lowering-options=" + List(
@@ -16,5 +50,9 @@ val outputPath = "/home/kong/ysyx-workbench/npc/build-sv/rvproc/mcRvCore.sv"
     ).reduce(_ + "," + _)
   )
 
-  circt.stage.ChiselStage.emitSystemVerilogFile(new rvproc.rvCore(true, iCacheConf(32, 128, 16, 1)), args, firtoolOptions)
+  circt.stage.ChiselStage.emitSystemVerilogFile(
+    new rvproc.rvCore(true, l1iConf),
+    rest.toArray,
+    firtoolOptions
+  )
 }
--- npc/rvproc/src/Elaborate.scala
+++ npc/rvproc/src/Elaborate.scala
@@ -2,9 +2,15 @@ import scala.util.Properties
 import java.nio.file.Paths
 import rvproc.cache.iCacheConf
 import scala.collection.mutable.ArrayBuffer
+import rvproc.GlbCtrl
 
 object Elaborate extends App {
   println(s"-> Elaborate Argv: ${args.mkString(":")}")
+
+  // Defaults for simulation: debug=true, sta=false
+  GlbCtrl.debug = true
+  GlbCtrl.sta = false
+  GlbCtrl.sramlib = false
 
   def parseArgs(args: Array[String]) = {
     var isSocMode  = false
@@ -20,6 +26,12 @@ object Elaborate extends App {
         case "--l1i-size"    => l1iSize = args(i + 1).toInt; i += 1
         case "--l1i-blksize" => l1iBlksize = args(i + 1).toInt; i += 1
         case "--l1i-assoc"   => l1iAssoc = args(i + 1).toInt; i += 1
+        case "--debug"       => GlbCtrl.debug = true
+        case "--no-debug"    => GlbCtrl.debug = false
+        case "--sta"         => GlbCtrl.sta = true
+        case "--no-sta"      => GlbCtrl.sta = false
+        case "--sramlib"     => GlbCtrl.sramlib = true
+        case "--no-sramlib"  => GlbCtrl.sramlib = false
         case other           => rest += other
       }
       i += 1
