6bfb57ad9db2481890cb6b4643275a12d4206a00
tracer-ysyx <tracer@ysyx.org>
2026-02-27 02:14:28 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 21 03:10:52 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  02:14:28 up 10:32,  3 users,  load average: 4.86, 1.38, 0.67

--- npc/libs/sram/sram_1rw.blackbox.sv
+++ npc/libs/sram/sram_1rw.blackbox.sv
@@ -1,21 +1,21 @@
 (* blackbox *)
-module sram_1rw_25x8 (
+module sram_1rw_24x32 (
   input              clk0,
   input              csb0,
   input              web0,
-  input  [2:0] addr0,
-  input  [24:0] din0,
-  output [24:0] dout0
+  input  [4:0] addr0,
+  input  [23:0] din0,
+  output [23:0] dout0
 );
 endmodule
 
 (* blackbox *)
-module sram_1rw_256x8 (
+module sram_1rw_128x32 (
   input              clk0,
   input              csb0,
   input              web0,
-  input  [2:0] addr0,
-  input  [255:0] din0,
-  output [255:0] dout0
+  input  [4:0] addr0,
+  input  [127:0] din0,
+  output [127:0] dout0
 );
 endmodule
