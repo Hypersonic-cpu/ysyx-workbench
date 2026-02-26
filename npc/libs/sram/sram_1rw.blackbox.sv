e8a82321f02133ce9f91b4ca31a12ea395efd0b6
tracer-ysyx <tracer@ysyx.org>
2026-02-27 02:13:01 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 21 03:10:52 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  02:13:01 up 10:30,  3 users,  load average: 0.57, 0.40, 0.35

--- npc/libs/sram/sram_1rw.blackbox.sv
+++ npc/libs/sram/sram_1rw.blackbox.sv
@@ -1,20 +1,20 @@
 (* blackbox *)
-module sram_1rw_24x32 (
+module sram_1rw_26x8 (
   input              clk0,
   input              csb0,
   input              web0,
-  input  [4:0] addr0,
-  input  [23:0] din0,
-  output [23:0] dout0
+  input  [2:0] addr0,
+  input  [25:0] din0,
+  output [25:0] dout0
 );
 endmodule
 
 (* blackbox *)
-module sram_1rw_128x32 (
+module sram_1rw_128x8 (
   input              clk0,
   input              csb0,
   input              web0,
-  input  [4:0] addr0,
+  input  [2:0] addr0,
   input  [127:0] din0,
   output [127:0] dout0
 );
--- npc/libs/sram/sram_1rw.wrappers.sv
+++ npc/libs/sram/sram_1rw.wrappers.sv
@@ -1,31 +1,31 @@
-module sram_1rw_24x32 (
+module sram_1rw_26x8 (
   input              clk0,
   input              csb0,
   input              web0,
-  input  [4:0] addr0,
-  input  [23:0] din0,
-  output [23:0] dout0
+  input  [2:0] addr0,
+  input  [25:0] din0,
+  output [25:0] dout0
 );
   sram_1rw #(
-    .WORD_SIZE(24),
-    .NUM_WORDS(32)
+    .WORD_SIZE(26),
+    .NUM_WORDS(8)
   ) u0 (
     .clk0(clk0), .csb0(csb0), .web0(web0),
     .addr0(addr0), .din0(din0), .dout0(dout0)
   );
 endmodule
 
-module sram_1rw_128x32 (
+module sram_1rw_128x8 (
   input              clk0,
   input              csb0,
   input              web0,
-  input  [4:0] addr0,
+  input  [2:0] addr0,
   input  [127:0] din0,
   output [127:0] dout0
 );
   sram_1rw #(
     .WORD_SIZE(128),
-    .NUM_WORDS(32)
+    .NUM_WORDS(8)
   ) u0 (
     .clk0(clk0), .csb0(csb0), .web0(web0),
     .addr0(addr0), .din0(din0), .dout0(dout0)
