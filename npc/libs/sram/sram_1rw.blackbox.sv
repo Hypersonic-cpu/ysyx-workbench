7c2a97cc7c4a51d80270ffc8ea7f8cf122b5eec3
tracer-ysyx <tracer@ysyx.org>
2026-03-01 01:24:04 +0800

    > generate verilog ysyx_25100264 孔一江 Linux kong-VMware20-1 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 21 03:10:52 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  01:24:04 up 1 day, 23:43,  3 users,  load average: 0.79, 0.65, 0.59

--- npc/libs/sram/sram_1rw.blackbox.sv
+++ npc/libs/sram/sram_1rw.blackbox.sv
@@ -10,22 +10,22 @@ module sram_1rw_22x64 (
 endmodule
 
 (* blackbox *)
-module sram_1rw_23x128 (
+module sram_1rw_25x32 (
   input              clk0,
   input              csb0,
   input              web0,
-  input  [6:0] addr0,
-  input  [22:0] din0,
-  output [22:0] dout0
+  input  [4:0] addr0,
+  input  [24:0] din0,
+  output [24:0] dout0
 );
 endmodule
 
 (* blackbox *)
-module sram_1rw_32x128 (
+module sram_1rw_32x32 (
   input              clk0,
   input              csb0,
   input              web0,
-  input  [6:0] addr0,
+  input  [4:0] addr0,
   input  [31:0] din0,
   output [31:0] dout0
 );
--- npc/libs/sram/sram_1rw.wrappers.sv
+++ npc/libs/sram/sram_1rw.wrappers.sv
@@ -15,34 +15,34 @@ module sram_1rw_22x64 (
   );
 endmodule
 
-module sram_1rw_23x128 (
+module sram_1rw_25x32 (
   input              clk0,
   input              csb0,
   input              web0,
-  input  [6:0] addr0,
-  input  [22:0] din0,
-  output [22:0] dout0
+  input  [4:0] addr0,
+  input  [24:0] din0,
+  output [24:0] dout0
 );
   sram_1rw #(
-    .WORD_SIZE(23),
-    .NUM_WORDS(128)
+    .WORD_SIZE(25),
+    .NUM_WORDS(32)
   ) u0 (
     .clk0(clk0), .csb0(csb0), .web0(web0),
     .addr0(addr0), .din0(din0), .dout0(dout0)
   );
 endmodule
 
-module sram_1rw_32x128 (
+module sram_1rw_32x32 (
   input              clk0,
   input              csb0,
   input              web0,
-  input  [6:0] addr0,
+  input  [4:0] addr0,
   input  [31:0] din0,
   output [31:0] dout0
 );
   sram_1rw #(
     .WORD_SIZE(32),
-    .NUM_WORDS(128)
+    .NUM_WORDS(32)
   ) u0 (
     .clk0(clk0), .csb0(csb0), .web0(web0),
     .addr0(addr0), .din0(din0), .dout0(dout0)
